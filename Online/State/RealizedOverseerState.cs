07d427ebb59e27fb3293c53a2548cb5cc5a769bb
DevLope <108824595+Dev-Lope@users.noreply.github.com>
2023-08-03 13:23:51 +0200

    Updated todo comments

--- Online/State/RealizedOverseerState.cs
+++ Online/State/RealizedOverseerState.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 namespace RainMeadow
 {
-    // oh my god overseers are terrible
-    // seriously
-
     // TODO: The overseer type isnt synced. (or atleast the guide, havent tested others).
     //          we could technically do this here if we didnt mind resending the same information every packet lmao.
-    // TODO: Conversations dont work, remote conversations dont play the animation.
-    // TODO: Remote overseers' mycelium (the lil tentacles) dont get moved along in a zip
+    // TODO: When zipping, the body and the mycelium gets stretched along the zip.
+    //          Should be fixable by ensuring DrawPosOfSegment returns what we want, 
+    //          and throwing in a well timed reset on the graphics module
     // TODO: Death is completely broken, need to make Die or HitSomethingWithoutStopping an event.
     //          this leads to an overseer being fake killed on remote.
-    // TODO: Ask videocult to remove overseers from the game.
     public class RealizedOverseerState : RealizedCreatureState
     {
         
