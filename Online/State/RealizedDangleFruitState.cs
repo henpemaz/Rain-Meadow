347dc1176bb513d5e63fb3fd7ea88190592ec04a
chrometurtle <ben.hs.tam@gmail.com>
2023-08-20 21:01:26 -0400

    dangle fruit sync [wip]

---
+++ Online/State/RealizedDangleFruitState.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace RainMeadow
+{
+
+    public class RealizedDangleFruitState : RealizedPhysicalObjectState
+    {
+        public override RealizedPhysicalObjectState EmptyDelta() => new RealizedDangleFruitState();
+
+        public RealizedDangleFruitState() { }
+
+        public RealizedDangleFruitState(OnlinePhysicalObject onlineEntity) : base(onlineEntity)
+        {
+            var fruit = (DangleFruit)onlineEntity.apo.realizedObject;
+            if (fruit.stalk != null)
+            {
+                var stalk = fruit.stalk;
+                //stalk.stuckPos;
+                //stalk.ropeLength;
+                //this.stalk = new DangleFruit.Stalk(this, placeRoom, base.firstChunk.pos);
+                RainMeadow.Debug("we found a stalk!");
+            }
+
+        }
+
+        public override void ReadTo(OnlineEntity onlineEntity)
+        {
+            if (!onlineEntity.owner.isMe && onlineEntity.isPending) return;
+            base.ReadTo(onlineEntity);
+
+        }
+
+        public override StateType stateType => StateType.RealizedDangleFruitState;
+
+        public override void CustomSerialize(Serializer serializer)
+        {
+            base.CustomSerialize(serializer);
+        }
+
+        public override long EstimatedSize(bool inDeltaContext)
+        {
+            return base.EstimatedSize(inDeltaContext);
+        }
+
+        public override RealizedPhysicalObjectState Delta(RealizedPhysicalObjectState _other)
+        {
+            var other = (RealizedDangleFruitState)_other;
+            var delta = (RealizedDangleFruitState)base.Delta(_other);
+
+            return delta;
+        }
+
+        public override RealizedPhysicalObjectState ApplyDelta(RealizedPhysicalObjectState _other) {
+            var other = (RealizedDangleFruitState)_other;
+            var result = (RealizedDangleFruitState)base.ApplyDelta(_other);
+
+            return result;
+        }
+    }
+}
