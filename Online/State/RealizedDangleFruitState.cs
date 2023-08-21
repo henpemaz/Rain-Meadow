2ba03a189b048698c4fc295fbc95c7d5d2498932
chrometurtle <ben.hs.tam@gmail.com>
2023-08-20 22:08:57 -0400

    simple dangle fruit syncing

--- Online/State/RealizedDangleFruitState.cs
+++ Online/State/RealizedDangleFruitState.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 namespace RainMeadow
 {
-
+    // 
     public class RealizedDangleFruitState : RealizedPhysicalObjectState
     {
+        bool hasStalk = false;
         public override RealizedPhysicalObjectState EmptyDelta() => new RealizedDangleFruitState();
 
         public RealizedDangleFruitState() { }
@@ -16,19 +17,25 @@ namespace RainMeadow
             var fruit = (DangleFruit)onlineEntity.apo.realizedObject;
             if (fruit.stalk != null)
             {
-                var stalk = fruit.stalk;
-                //stalk.stuckPos;
-                //stalk.ropeLength;
-                //this.stalk = new DangleFruit.Stalk(this, placeRoom, base.firstChunk.pos);
-                RainMeadow.Debug("we found a stalk!");
+                this.hasStalk = true;
             }
 
         }
 
         public override void ReadTo(OnlineEntity onlineEntity)
         {
-            if (!onlineEntity.owner.isMe && onlineEntity.isPending) return;
+            if (!onlineEntity.owner.isMe && onlineEntity.isPending) return; // Don't sync if pending, reduces visibility and effect of lag
             base.ReadTo(onlineEntity);
+            var fruit = (DangleFruit)((OnlinePhysicalObject)onlineEntity).apo.realizedObject;
+            if (hasStalk && fruit.stalk == null)
+            {
+                if (fruit.stalk == null) {
+
+                    fruit.stalk = new DangleFruit.Stalk(fruit, fruit.room, fruit.firstChunk.pos);
+                    fruit.room.AddObject(fruit.stalk);
+
+                }
+            }
 
         }
 
@@ -37,25 +44,28 @@ namespace RainMeadow
         public override void CustomSerialize(Serializer serializer)
         {
             base.CustomSerialize(serializer);
+            serializer.Serialize(ref hasStalk);
         }
 
         public override long EstimatedSize(bool inDeltaContext)
         {
-            return base.EstimatedSize(inDeltaContext);
+            var size = base.EstimatedSize(inDeltaContext);
+            size += 1;
+            return size;
         }
 
         public override RealizedPhysicalObjectState Delta(RealizedPhysicalObjectState _other)
         {
             var other = (RealizedDangleFruitState)_other;
             var delta = (RealizedDangleFruitState)base.Delta(_other);
-
+            delta.hasStalk = this.hasStalk;
             return delta;
         }
 
         public override RealizedPhysicalObjectState ApplyDelta(RealizedPhysicalObjectState _other) {
             var other = (RealizedDangleFruitState)_other;
             var result = (RealizedDangleFruitState)base.ApplyDelta(_other);
-
+            result.hasStalk = other.hasStalk;
             return result;
         }
     }
