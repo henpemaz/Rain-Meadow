defd0834aef678c570c28a081a14f9a01356bdfb
Preston <pgibbs1@liberty.edu>
2024-03-04 13:17:21 -0500

    Removed things I'm not using

--- Story/OnlineUIComponents/OnlinePlayerIcon.cs
+++ Story/OnlineUIComponents/OnlinePlayerIcon.cs
@@ -2,13 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using HUD;
-using IL.HUD;
-using On.HUD;
 using System.Security.AccessControl;
-using JollyCoop;
 
 namespace RainMeadow
 {
@@ -17,37 +13,19 @@ namespace RainMeadow
 
 
         public FSprite iconSprite;
-
         public Color color;
-
         public Vector2 pos;
-
-        public Vector2 lastPos;
-
         public float blink;
-
         public int blinkRed;
-
         public bool dead;
-        public AbstractCreature abstractPlayer;
-
-        public Player RealizedPlayer => this.abstractPlayer.realizedCreature as Player;
-
         public float lastBlink;
         public float rad;
         public float alpha;
         public float lastAlpha;
-        public int counter;
-        public int fadeAwayCounter;
-
-        public Vector2 DrawPos()
-        {
-            return pos;
-        }
         public OnlinePlayerIcon(PlayerSpecificOnlineHud owner) : base(owner)
         {
             this.owner = owner;
-           
+
             RainWorldGame rainWorldGame = owner.hud.rainWorld.processManager.currentMainLoop as RainWorldGame;
 
             this.pos = new Vector2(rainWorldGame.rainWorld.options.ScreenSize.x - owner.hud.foodMeter.pos.x - 25f + (float)(3 - OnlineManager.players.Count), owner.hud.foodMeter.pos.y);
@@ -63,7 +41,7 @@ namespace RainMeadow
 
             iconSprite.alpha = Mathf.Pow(Mathf.Max(0f, Mathf.Lerp(this.lastAlpha, this.alpha, timeStacker)), 0.7f);
             iconSprite.y = owner.hud.foodMeter.pos.y;
-            iconSprite.x = DrawPos().x;
+            iconSprite.x = pos.x;
             iconSprite.color = owner.clientSettings.SlugcatColor();
         }
 
@@ -83,19 +61,19 @@ namespace RainMeadow
 
             }
             iconSprite.color = color;
-/*            if (RealizedPlayer.playerState.permaDead || (RealizedPlayer.playerState.dead))
-            {
-                color = Color.gray;
-                if (!dead)
-                {
-                    iconSprite.RemoveFromContainer();
-                    iconSprite = new FSprite("Multiplayer_Death");
-                    iconSprite.scale *= 0.8f;
-                    owner.hud.fContainers[0].AddChild(iconSprite);
-                    dead = true;
-                    blink = 3f;
-                }
-            }*/
+            /*            if (RealizedPlayer.playerState.permaDead || (RealizedPlayer.playerState.dead))
+                        {
+                            color = Color.gray;
+                            if (!dead)
+                            {
+                                iconSprite.RemoveFromContainer();
+                                iconSprite = new FSprite("Multiplayer_Death");
+                                iconSprite.scale *= 0.8f;
+                                owner.hud.fContainers[0].AddChild(iconSprite);
+                                dead = true;
+                                blink = 3f;
+                            }
+                        }*/
 
         }
     }
