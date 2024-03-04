8d3e102ceb044412be4e9e0cb23f57c2b3adfebb
sixfears7 <57415489+6fears7@users.noreply.github.com>
2024-03-04 18:36:37 -0500

    added death switch

--- Story/OnlineUIComponents/OnlinePlayerIcon.cs
+++ Story/OnlineUIComponents/OnlinePlayerIcon.cs
@@ -13,10 +13,7 @@ namespace RainMeadow
         public FSprite iconSprite;
         public Color color;
         public Vector2 pos;
-        public float blink;
-        public int blinkRed;
         public bool dead;
-        public float lastBlink;
         public float rad;
         public float alpha;
         public float lastAlpha;
@@ -28,7 +25,7 @@ namespace RainMeadow
 
             this.pos = new Vector2(rainWorldGame.rainWorld.options.ScreenSize.x - owner.hud.foodMeter.pos.x - 25f + (float)(3 - OnlineManager.players.Count), owner.hud.foodMeter.pos.y);
             this.iconSprite = new FSprite("Kill_Slugcat");
-
+            this.dead = false;
             this.color = owner.clientSettings.SlugcatColor();
             owner.hud.fContainers[0].AddChild(iconSprite);
 
@@ -38,7 +35,7 @@ namespace RainMeadow
         {
 
             iconSprite.alpha = Mathf.Pow(Mathf.Max(0f, Mathf.Lerp(this.lastAlpha, this.alpha, timeStacker)), 0.7f);
-            iconSprite.y = owner.drawpos.y + 85f;
+            iconSprite.y = owner.drawpos.y + 87f;
             iconSprite.x = owner.drawpos.x;
             iconSprite.color = owner.clientSettings.SlugcatColor();
         }
@@ -46,10 +43,24 @@ namespace RainMeadow
         public override void Update()
         {
             base.Update();
+            iconSprite.color = color;
+
             if (Input.GetKey(RainMeadow.rainMeadowOptions.FriendsListKey.Value))
             {
                 this.lastAlpha = this.alpha;
                 this.alpha = RWCustom.Custom.LerpAndTick(this.alpha, owner.needed ? 1 : 0, 0.08f, 0.033333335f);
+
+                if (owner.PlayerConsideredDead)
+                {
+                    this.alpha = 0.5f;
+                    if (!dead)
+                    {
+                        iconSprite.RemoveFromContainer();
+                        iconSprite = new FSprite("Multiplayer_Death");
+                        owner.hud.fContainers[0].AddChild(iconSprite);
+                        dead = true;
+                    }
+                }
 
             }
             else
@@ -58,20 +69,7 @@ namespace RainMeadow
                 this.lastAlpha = this.alpha;
 
             }
-            iconSprite.color = color;
-            /*            if (RealizedPlayer.playerState.permaDead || (RealizedPlayer.playerState.dead))
-                        {
-                            color = Color.gray;
-                            if (!dead)
-                            {
-                                iconSprite.RemoveFromContainer();
-                                iconSprite = new FSprite("Multiplayer_Death");
-                                iconSprite.scale *= 0.8f;
-                                owner.hud.fContainers[0].AddChild(iconSprite);
-                                dead = true;
-                                blink = 3f;
-                            }
-                        }*/
+
 
         }
     }
