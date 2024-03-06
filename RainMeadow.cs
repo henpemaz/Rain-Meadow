4e354ea32b4d63a77afafc8b783e514a32032c16
SaddestUnderscore <nsaber3911@gmail.com>
2024-03-07 00:11:10 +0300

    error fix for local builds

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -176,7 +176,7 @@ namespace RainMeadow
 #if LOCAL_P2P
                 if (!self.setup.startScreen)
                 {
-                    OnlineManager.lobby = new Lobby(new OnlineGameMode.OnlineGameModeType(LocalMatchmakingManager.localGameMode), OnlineManager.mePlayer, null, 4);
+                    OnlineManager.lobby = new Lobby(new OnlineGameMode.OnlineGameModeType(LocalMatchmakingManager.localGameMode), OnlineManager.mePlayer, null);
                 }
 #endif
             }
