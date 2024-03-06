c0cff57e07d1880019ab5f3d89573a094fb431c6
chrometurtle <ben.hs.tam@gmail.com>
2024-03-05 20:15:02 -0500

    match other if statements for Danglefruit in PhysicalObjectEntityState

--- Online/State/PhysicalObjectEntityState.cs
+++ Online/State/PhysicalObjectEntityState.cs
@@ -42,7 +42,7 @@ namespace RainMeadow
             if (onlineObject.apo.realizedObject is SlimeMold) return new RealizedSlimeMoldState(onlineObject);
             if (onlineObject.apo.realizedObject is VultureGrub) return new RealizedVultureGrubState(onlineObject);
             if (onlineObject.apo.realizedObject is SeedCob) return new RealizedSeedCobState(onlineObject);
-            if (onlineObject.apo.realizedObject is DangleFruit) { return new RealizedDangleFruitState(onlineObject); }
+            if (onlineObject.apo.realizedObject is DangleFruit) return new RealizedDangleFruitState(onlineObject);
             return new RealizedPhysicalObjectState(onlineObject);
         }
 
