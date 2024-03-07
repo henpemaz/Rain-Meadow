304dfc10b9d9820ff29d2e2ca4b16830b50ab15b
chrometurtle <ben.hs.tam@gmail.com>
2024-03-07 17:44:27 -0500

    add back in onlineBubbleGrass

---
+++ Online/Entity/OnlineBubbleGrass.cs
@@ -0,0 +1,35 @@
+﻿using static RainMeadow.OnlineEntity;
+
+namespace RainMeadow
+{
+    public class OnlineBubbleGrass : OnlineConsumable
+    {
+        public OnlineBubbleGrass(OnlineBubbleGrassDefinition entityDefinition, AbstractPhysicalObject apo) : base(entityDefinition, apo) { }
+
+        public BubbleGrass.AbstractBubbleGrass AbstractBubbleGrass => apo as BubbleGrass.AbstractBubbleGrass;
+
+        public static OnlineEntity FromDefinition(OnlineConsumableDefinition newObjectEvent, OnlineResource inResource)
+        {
+            World world = inResource is RoomSession rs ? rs.World : inResource is WorldSession ws ? ws.world : throw new InvalidProgrammerException("not room nor world");
+            EntityID id = world.game.GetNewID();
+            id.altSeed = newObjectEvent.seed;
+
+            RainMeadow.Debug("serializedObject: " + newObjectEvent.serializedObject);
+            var acm = (AbstractConsumable)SaveState.AbstractPhysicalObjectFromString(world, newObjectEvent.serializedObject);
+            acm.ID = id;
+
+            return new OnlineConsumable(newObjectEvent, acm);
+        }
+
+        protected override EntityState MakeState(uint tick, OnlineResource inResource)
+        {
+            return new OnlineConsumableState(this, inResource, tick);
+        }
+
+        public override void OnJoinedResource(OnlineResource inResource)
+        {
+            base.OnJoinedResource(inResource);
+            // ?
+        }
+    }
+}
---
+++ Online/Entity/OnlineBubbleGrassDefinition.cs
@@ -0,0 +1,19 @@
+﻿namespace RainMeadow;
+    public class OnlineBubbleGrassDefinition : OnlineConsumableDefinition
+    {
+        [OnlineField]
+        public float originalOxygenLevel;
+
+        public OnlineBubbleGrassDefinition() { }
+
+        public OnlineBubbleGrassDefinition(OnlineConsumableDefinition opod, BubbleGrass.AbstractBubbleGrass abstractConsumable) : base(opod)
+        {
+            this.originalOxygenLevel = (short)abstractConsumable.oxygenLeft;
+        }
+
+        public override OnlineEntity MakeEntity(OnlineResource inResource)
+        {
+            return OnlineBubbleGrass.FromDefinition(this, inResource);
+        }
+    }
+}
