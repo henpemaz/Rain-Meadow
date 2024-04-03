6a454f215d2db94adc231316391ab0264151e435
chrometurtle <ben.hs.tam@gmail.com>
2024-04-03 14:02:45 -0400

    sync puffball explode

--- Online/ConsumableRPCs.cs
+++ Online/ConsumableRPCs.cs
@@ -1,4 +1,6 @@
-﻿namespace RainMeadow
+﻿using UnityEngine;
+
+namespace RainMeadow
 {
     public static class ConsumableRPCs
     {
@@ -19,6 +21,32 @@
         [RPCMethod]
         public static void pacifySporePlant(OnlinePhysicalObject onlineSporePlant) {
             (onlineSporePlant.apo.realizedObject as SporePlant).Pacify(); 
+        }
+
+        //This may add a damage multiplier, based on how many players are in the same room, to insects.
+        [RPCMethod]
+        public static void explodePuffBall(RoomSession onlineRoom, Vector2 pos, Color sporeColor, Color puffballColor) 
+        {
+            var room = onlineRoom.absroom.realizedRoom;
+            InsectCoordinator smallInsects = null;
+            for (int i = 0; i < room.updateList.Count; i++)
+            {
+                if (room.updateList[i] is InsectCoordinator)
+                {
+                    smallInsects = (room.updateList[i] as InsectCoordinator);
+                    break;
+                }
+            }
+            for (int j = 0; j < 70; j++)
+            {
+                room.AddObject(new SporeCloud(pos, RWCustom.Custom.RNV() * Random.value * 10f, sporeColor, 1f, null, j % 20, smallInsects));
+            }
+            room.AddObject(new SporePuffVisionObscurer(pos));
+            for (int k = 0; k < 7; k++)
+            {
+                room.AddObject(new PuffBallSkin(pos, RWCustom.Custom.RNV() * Random.value * 16f, puffballColor, Color.Lerp(puffballColor, sporeColor, 0.5f)));
+            }
+            room.PlaySound(SoundID.Puffball_Eplode, pos);
         }
     }
 }
--- Story/RainMeadow.StoryHooks.cs
+++ Story/RainMeadow.StoryHooks.cs
@@ -46,9 +46,44 @@ namespace RainMeadow
             On.WaterNut.Swell += WaterNut_Swell;
             On.SporePlant.Pacify += SporePlant_Pacify;
 
+            On.PuffBall.Explode += PuffBall_Explode;
+
             On.Oracle.CreateMarble += Oracle_CreateMarble;
             On.Oracle.SetUpMarbles += Oracle_SetUpMarbles;
 
+        }
+
+        private void PuffBall_Explode(On.PuffBall.orig_Explode orig, PuffBall self)
+        {
+            if (OnlineManager.lobby == null)
+            {
+                orig(self);
+                return;
+            }
+
+            RoomSession.map.TryGetValue(self.room.abstractRoom, out var onlineRoom);
+            OnlinePhysicalObject.map.TryGetValue(self.abstractPhysicalObject, out var onlineSporePlant);
+
+            if (onlineSporePlant.isMine) 
+            {
+                foreach (var kv in OnlineManager.lobby.playerAvatars)
+                {
+                    var playerAvatar = kv.Value;
+                    if (playerAvatar.type == (byte)OnlineEntity.EntityId.IdType.none || kv.Key.isMe) continue; // not in game or is me
+                    if (playerAvatar.FindEntity(true) is OnlinePhysicalObject opo && opo.apo is AbstractCreature ac)
+                    {
+                        if (ac.Room == self.room.abstractRoom)
+                        {
+                            if (!opo.owner.OutgoingEvents.Any(e => e is RPCEvent rpc && rpc.IsIdentical(ConsumableRPCs.explodePuffBall, onlineSporePlant, self.bodyChunks[0].pos))) 
+                            {
+                                opo.owner.InvokeRPC(ConsumableRPCs.explodePuffBall, onlineRoom, self.bodyChunks[0].pos, self.sporeColor, self.color);
+                            }
+                        }
+                    }
+                }
+                orig(self);
+                return;
+            }
         }
 
         private void Oracle_SetUpMarbles(On.Oracle.orig_SetUpMarbles orig, Oracle self)
