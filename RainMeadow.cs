f1535745627ed0cd2d7671bfe604dd412e559eb9
chrometurtle <ben.hs.tam@gmail.com>
2024-04-08 14:04:04 -0400

    bump version to 0.0.60

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -14,7 +14,7 @@ namespace RainMeadow
     [BepInPlugin("henpemaz.rainmeadow", "RainMeadow", MeadowVersionStr)]
     public partial class RainMeadow : BaseUnityPlugin
     {
-        public const string MeadowVersionStr = "0.0.59";
+        public const string MeadowVersionStr = "0.0.60";
         public static RainMeadow instance;
         private bool init;
         public static RainMeadowOptions rainMeadowOptions;
