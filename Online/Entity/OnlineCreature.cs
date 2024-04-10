136ee73fb7814228048106a320862dcfe4457576
sixfears7 <57415489+6fears7@users.noreply.github.com>
2024-04-10 10:54:52 -0400

    adding work for now. Still investigating error in Player_ctor

--- Online/Entity/OnlineCreature.cs
+++ Online/Entity/OnlineCreature.cs
@@ -71,6 +71,32 @@ namespace RainMeadow
 
             int? num = BackwardsCompatibilityRemix.ParseRoomIndex(array2[0]);
 
+            /*            if (RainMeadow.isArenaMode(out var _))
+                        {
+
+                            int num2 = world.GetAbstractRoom("arenasmallroom").index;
+                            EntityID id2 = EntityID.FromString(array[1]);
+
+                            WorldCoordinate denz = new WorldCoordinate(num2, -1, 1, int.Parse(array2[1], NumberStyles.Any, CultureInfo.InvariantCulture));
+
+                            AbstractCreature abstractCreature2 = new AbstractCreature(world, StaticWorld.GetCreatureTemplate(type), null, denz, id2);
+                            abstractCreature2.state.alive = true;
+                            abstractCreature2.setCustomFlags();
+
+                            return abstractCreature2;
+
+                        }*/
+
+            foreach (var thing in array)
+            {
+                RainMeadow.Debug("ARRAY 1 " + thing);
+            }
+
+            foreach (var thing2 in array2)
+            {
+                RainMeadow.Debug("ARRAY TWO " + thing2);
+            }
+
             if (num == null || !world.IsRoomInRegion(num.Value))
             {
 
@@ -104,6 +130,7 @@ namespace RainMeadow
 
         protected override AbstractPhysicalObject ApoFromDef(OnlinePhysicalObjectDefinition newObjectEvent, OnlineResource inResource, PhysicalObjectEntityState initialState)
         {
+            RainMeadow.Debug("ur mom");
             World world = inResource is RoomSession rs ? rs.World : inResource is WorldSession ws ? ws.world : throw new InvalidProgrammerException("not room nor world");
             EntityID id = world.game.GetNewID();
 
