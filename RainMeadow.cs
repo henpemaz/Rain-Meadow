ceaf9d3bf24edb45a98a2d95b8d1357681748271
Henrique Maziero <henpemaz@gmail.com>
2024-07-24 22:20:20 +0200

    Wrapped object update calls in trycatch

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -192,6 +192,7 @@ namespace RainMeadow
                 StoryHooks();
                 ArenaHooks();
                 ItemHooks();
+                ObjectHooks();
 
                 MeadowMusic.EnableMusic();
 
