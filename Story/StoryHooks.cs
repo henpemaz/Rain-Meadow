7fa1a44f5b34189867eb67239d2068990b8f4772
6fears7 <6fears7even@gmail.com>
2024-09-04 17:10:36 -0400

    better camera logic

--- Story/StoryHooks.cs
+++ Story/StoryHooks.cs
@@ -73,31 +73,31 @@ namespace RainMeadow
 
             if (OnlineManager.lobby.gameMode is StoryGameMode)
             {
-                var result = self.game.AlivePlayers;
+                if (self.game.cameras[0].hud.textPrompt.gameOverMode)
+                {
+                    self.game.cameras[0].hud.textPrompt.gameOverMode = false;
+                }
+
                 List<AbstractCreature> acList = new List<AbstractCreature>();
 
                 foreach (var playerAvatar in OnlineManager.lobby.playerAvatars.Values)
                 {
-                    if (playerAvatar.type == (byte)OnlineEntity.EntityId.IdType.none) continue; // not in game
+                    if (playerAvatar.type == (byte)OnlineEntity.EntityId.IdType.none) continue;
                     if (playerAvatar.FindEntity(true) is OnlinePhysicalObject opo && opo.apo is AbstractCreature ac)
                     {
-                        if (ac.state.alive && ac.realizedCreature != null)
-                        {
-                            acList.Add(ac);
-                        }
+
+                        acList.Add(ac);
+
                     }
                 }
 
 
                 for (int i = 0; i < acList.Count; i++)
                 {
-                    OnlinePhysicalObject.map.TryGetValue(acList[i].realizedCreature.abstractPhysicalObject, out var alivePlayer);
-
-
                     var username = "";
-
                     try
                     {
+                        OnlinePhysicalObject.map.TryGetValue(acList[i], out var alivePlayer);
                         username = (alivePlayer.owner.id as SteamMatchmakingManager.SteamPlayerId).name;
                     }
                     catch
@@ -105,33 +105,39 @@ namespace RainMeadow
                         username = $"{acList[i]}";
                     };
 
-
-                    self.pages[0].subObjects.Add(new Menu.MenuLabel(self, self.pages[0], self.Translate("FOUND PLAYERS"), new Vector2(1190, 553), new(110, 30), true));
+                    self.pages[0].subObjects.Add(new Menu.MenuLabel(self, self.pages[0], self.Translate("PLAYERS"), new Vector2(1190, 553), new(110, 30), true));
                     var btn = new SimplerButton(self, self.pages[0], username, new Vector2(1190, 515) - i * new Vector2(0, 38), new(110, 30));
                     self.pages[0].subObjects.Add(btn);
                     btn.toggled = false;
 
                     // Introduce a local variable to hold the current index
-                    int currentIdx = i;
-
-                    if (self.game.cameras[0].room.abstractRoom == acList[currentIdx].Room || acList[currentIdx].Room.realizedRoom == null)
+                    int currentCreature = i;
+                    if (acList[currentCreature].state.dead)
                     {
                         btn.inactive = true;
+                        btn.OnClick += (_) =>
+                        {
+                            return;
+                        };
                     }
                     else
                     {
                         btn.inactive = false;
-                    }
-
-
-                    btn.OnClick += (_) =>
-                    {
-
-                        if ((self.game.cameras[0].room.abstractRoom != acList[currentIdx].Room) && acList[currentIdx].Room.realizedRoom != null)
+                        btn.OnClick += (_) =>
                         {
-                            self.game.cameras[0].followAbstractCreature = acList[currentIdx];
-                            RainMeadow.Debug("Following " + acList[currentIdx]);
-                            self.game.cameras[0].MoveCamera(acList[currentIdx].Room.realizedRoom, -1);
+
+                            if (acList[currentCreature].Room.realizedRoom == null)
+                            {
+                                self.game.world.ActivateRoom(acList[currentCreature].Room);
+                            }
+                            self.game.cameras[0].followAbstractCreature = acList[currentCreature];
+
+                            RainMeadow.Debug("Following " + acList[currentCreature]);
+
+                            if ((self.game.cameras[0].room.abstractRoom != acList[currentCreature].Room))
+                            {
+                                self.game.cameras[0].MoveCamera(acList[currentCreature].Room.realizedRoom, -1);
+                            }
                             btn.toggled = !btn.toggled;
 
 
@@ -143,11 +149,8 @@ namespace RainMeadow
                                     otherBtn.toggled = false;
                                 }
                             }
-                        }
-                        else
-                        {
-                            return;
-                        }
+
+                        };
                     };
                 }
             }
