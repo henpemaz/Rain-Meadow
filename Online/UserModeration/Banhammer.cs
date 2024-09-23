06e65528c97a2f9a32be2df67ba2393a42235968
6fears7 <6fears7even@gmail.com>
2024-09-23 17:45:40 -0400

    fixed notify dialog. Just need to update the lobby state

--- Online/UserModeration/Banhammer.cs
+++ Online/UserModeration/Banhammer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using HUD;
 using Menu;
 using UnityEngine;
 
@@ -15,17 +16,11 @@ namespace RainMeadow
                 manager.dialog = null;
             };
 
-            Action cancelProceed = () =>
-            {
-                manager.dialog = null;
-
-
-            };
-
-            DialogConfirm informBadUser = new DialogConfirm("You were kicked from the previous game", manager, confirmProceed, cancelProceed);
+            DialogNotify informBadUser = new DialogNotify("You were removed from the previous online game", manager, confirmProceed);
 
 
             manager.ShowDialog(informBadUser);
+            
 
         }
 
