816da34ff6f94e2182c53658c7cf88f6feeb890e
choc <notchoc@disroot.org>
2024-10-01 07:25:30 +0800

    add RealizedSingularityBombState.cs
    oops

---
+++ Online/State/RealizedSingularityBombState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+namespace RainMeadow
+{
+    public class RealizedSingularityBombState : RealizedWeaponState
+    {
+        [OnlineField]
+        bool ignited;
+
+        [OnlineField]
+        bool activateSingularity;
+
+        [OnlineField]
+        bool activateSucktion;
+
+        [OnlineField]
+        byte counter; // float is always used as an integer with range [0, 120)
+
+        public RealizedSingularityBombState() { }
+        public RealizedSingularityBombState(OnlinePhysicalObject onlineEntity) : base(onlineEntity)
+        {
+            var bomb = (MoreSlugcats.SingularityBomb)onlineEntity.apo.realizedObject;
+            ignited = bomb.ignited;
+            activateSingularity = bomb.activateSingularity;
+            activateSucktion = bomb.activateSucktion;
+            counter = (byte)bomb.counter;
+        }
+
+        public override void ReadTo(OnlineEntity onlineEntity)
+        {
+            base.ReadTo(onlineEntity);
+            var bomb = (MoreSlugcats.SingularityBomb)((OnlinePhysicalObject)onlineEntity).apo.realizedObject;
+            bomb.ignited = ignited;
+            bomb.activateSingularity = activateSingularity;
+            bomb.activateSucktion = activateSucktion;
+            bomb.counter = counter;
+        }
+    }
+}
