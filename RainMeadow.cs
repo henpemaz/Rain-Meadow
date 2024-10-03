f8dcc718f3b494febe5ff7f73567cb24d6194cf5
Henrique Maziero <henpemaz@gmail.com>
2024-10-03 19:24:47 +0200

    version bump

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -14,7 +14,7 @@ namespace RainMeadow
     [BepInPlugin("henpemaz.rainmeadow", "RainMeadow", MeadowVersionStr)]
     public partial class RainMeadow : BaseUnityPlugin
     {
-        public const string MeadowVersionStr = "0.0.71";
+        public const string MeadowVersionStr = "0.0.72";
         public static RainMeadow instance;
         private bool init;
         public bool fullyInit;
