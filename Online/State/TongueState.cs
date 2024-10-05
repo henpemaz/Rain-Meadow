6c6ecefa13f10cc56b2606e66d0b46ae0a05a48c
choc <notchoc@disroot.org>
2024-10-05 15:10:30 +0800

    sync grapple worms

---
+++ Online/State/TongueState.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+namespace RainMeadow
+{
+    // ideally we should reuse this for Player.Tongue since all the fields are the same
+    // unfortunately they straight up copypasted the code
+    // so TubeWorm.Tongue and Player.Tongue are two entirely different types
+    // Thanks, Andrew -,-
+    [DeltaSupport(level = StateHandler.DeltaSupport.FollowsContainer)]
+    public class TongueState : OnlineState
+    {
+        [OnlineField]
+        public byte mode;
+        [OnlineField]
+        public Vector2 pos;
+        //[OnlineFieldHalf]
+        //public float idealRopeLength; // used by Player.Tongue, not modified by TubeWorm
+        [OnlineFieldHalf]
+        public float requestedRopeLength;
+        [OnlineField(nullable = true)]
+        public OnlineEntity.EntityId? attachedObject;
+        [OnlineField]
+        public byte attachedChunkIndex;
+
+        public TongueState() { }
+        public TongueState(TubeWorm.Tongue tongue)
+        {
+            mode = (byte)tongue.mode;
+            pos = tongue.pos;
+            //idealRopeLength = tongue.idealRopeLength;
+            requestedRopeLength = tongue.requestedRopeLength;
+            if (tongue.attachedChunk?.owner?.abstractPhysicalObject is AbstractPhysicalObject apo
+                && OnlinePhysicalObject.map.TryGetValue(apo, out var oe))
+            {
+                attachedObject = oe.id;
+                attachedChunkIndex = (byte)tongue.attachedChunk.index;
+            }
+        }
+
+        public void ReadTo(TubeWorm.Tongue tongue)
+        {
+            tongue.mode = new TubeWorm.Tongue.Mode(TubeWorm.Tongue.Mode.values.GetEntry(mode));
+            tongue.pos = pos;
+            //tongue.idealRopeLength = idealRopeLength;
+            tongue.requestedRopeLength = requestedRopeLength;
+            if (tongue.mode == TubeWorm.Tongue.Mode.AttachedToTerrain)
+            {
+                tongue.terrainStuckPos = tongue.pos;
+            }
+            else if (tongue.mode == TubeWorm.Tongue.Mode.AttachedToObject
+                && (attachedObject?.FindEntity() as OnlinePhysicalObject)?.apo?.realizedObject is PhysicalObject po)
+            {
+                tongue.attachedChunk = po.bodyChunks[attachedChunkIndex];
+            }
+        }
+    }
+}
