65ced1b5c2a7970784649ef3b06940204b33d819
SaddestUnderscore <nsaber3911@gmail.com>
2024-10-16 23:39:06 +0300

    Separate text box into its own remix toggle this time

--- Story/StoryHooks.cs
+++ Story/StoryHooks.cs
@@ -467,7 +467,7 @@ namespace RainMeadow
                 self.AddPart(new OnlineHUD(self, cam, gameMode));
                 self.AddPart(new SpectatorHud(self, cam, gameMode));
                 self.AddPart(new Pointing(self));
-
+                self.AddPart(new ChatHud(self, cam, gameMode));
             }
         }
 
@@ -696,7 +696,7 @@ namespace RainMeadow
                 self.room.game.cameras[0].hud.parts.Add(new OnlineHUD(self.room.game.cameras[0].hud, self.room.game.cameras[0], storyGameMode));
                 self.room.game.cameras[0].hud.parts.Add(new SpectatorHud(self.room.game.cameras[0].hud, self.room.game.cameras[0], storyGameMode));
                 self.room.game.cameras[0].hud.parts.Add(new Pointing(self.room.game.cameras[0].hud));
-
+                self.room.game.cameras[0].hud.parts.Add(new ChatHud(self.room.game.cameras[0].hud, self.room.game.cameras[0], storyGameMode));
 
                 return true;
             }
