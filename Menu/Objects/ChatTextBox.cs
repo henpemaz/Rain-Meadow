e45694f0d6857a351d5a71e099aaa4cc819131e4
SaddestUnderscore <nsaber3911@gmail.com>
2024-10-16 23:39:06 +0300

    Separate text box into its own remix toggle this time

--- Menu/Objects/ChatTextBox.cs
+++ Menu/Objects/ChatTextBox.cs
@@ -15,7 +15,8 @@ namespace RainMeadow
         public string value;
         public ChatTextBox(Menu.Menu menu, MenuObject owner, string displayText, Vector2 pos, Vector2 size) : base(menu, owner, displayText, "", pos, size)
         {
-            gameObject ??= new GameObject("typingHandler");
+            this.menu = menu;
+            gameObject ??= new GameObject();
             OnKeyDown = (Action<char>)Delegate.Combine(OnKeyDown, new Action<char>(CaptureInputs));
             if (typingHandler == null)
             {
@@ -29,12 +30,23 @@ namespace RainMeadow
             {
                 if (value.Length > 0)
                 {
+                    menu.PlaySound(SoundID.MENY_Already_Selected_MultipleChoice_Clicked);
                     value = value.Substring(0, value.Length - 1);
                 }
             }
             else
             {
+                menu.PlaySound(SoundID.MENU_Checkbox_Check);
                 value += input.ToString();
+            }
+
+            if (value.Length > 0)
+            {
+                if (input == '\n' || input == '\r')
+                {
+                    typingHandler.Unassign(this);
+                    RainMeadow.Debug(value);
+                }
             }
             menuLabel.text = value;
         }
---
+++ Story/OnlineUIComponents/ChatHud.cs
@@ -0,0 +1,55 @@
+﻿using HUD;
+using UnityEngine;
+
+namespace RainMeadow
+{
+    public class ChatHud : HudPart
+    {
+
+        private RoomCamera camera;
+        private readonly OnlineGameMode onlineGameMode;
+        private Menu.Menu chatBox;
+        private RainWorldGame game;
+        private bool chatActive = false;
+
+
+        public ChatHud(HUD.HUD hud, RoomCamera camera, OnlineGameMode onlineGameMode) : base(hud)
+        {
+            this.camera = camera;
+            this.onlineGameMode = onlineGameMode;
+            game = camera.game;
+        }
+
+        public override void Draw(float timeStacker)
+        {
+            base.Draw(timeStacker);
+            if (Input.GetKeyDown(RainMeadow.rainMeadowOptions.ChatKey.Value) && chatBox == null && chatActive == false)
+            {
+                RainMeadow.Debug("creating chat box");
+                chatBox = new ChatOverlay(game.manager, game);
+                chatActive = true;
+            }
+
+            chatBox?.GrafUpdate(timeStacker);
+
+            if (Input.GetKeyDown(KeyCode.Return) && chatBox != null) ShutDownChat();
+        }
+        public void ShutDownChat()
+        {
+            RainMeadow.Debug("shut down chat box");
+            chatBox.ShutDownProcess();
+            chatBox = null;
+            chatActive = false;
+        }
+        public override void Update()
+        {
+            base.Update();
+            if (OnlineManager.lobby.gameMode is StoryGameMode)
+            {
+                if ((game.pauseMenu != null || camera.hud.map.visible || game.manager.upcomingProcess != null) && chatBox != null) ShutDownChat();
+
+                chatBox?.Update();
+            }
+        }
+    }
+}
