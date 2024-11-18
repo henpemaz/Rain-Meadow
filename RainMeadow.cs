1327aaf2db1870807fd43e1fe5a8c26666fb9e58
6fears7 <6fears7even@gmail.com>
2024-11-18 07:03:45 -0500

    version bump for hotfix

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -14,7 +14,7 @@ namespace RainMeadow
     [BepInPlugin("henpemaz.rainmeadow", "RainMeadow", MeadowVersionStr)]
     public partial class RainMeadow : BaseUnityPlugin
     {
-        public const string MeadowVersionStr = "0.0.74";
+        public const string MeadowVersionStr = "0.0.74.1";
         public static RainMeadow instance;
         private bool init;
         public bool fullyInit;
