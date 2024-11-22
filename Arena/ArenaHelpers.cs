0ee32a406e0ff2f863d0050e30a56120b2113787
6fears7 <6fears7even@gmail.com>
2024-11-22 16:48:17 -0500

    stuck in IL

--- Arena/ArenaHelpers.cs
+++ Arena/ArenaHelpers.cs
@@ -151,19 +151,27 @@ namespace RainMeadow
         }
         public static void OverideSlugcatClassAbilities(Player player, ArenaCompetitiveGameMode arena)
         {
+            if (player.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Saint) {
+                //if (saintAscensionInArena)
+                //{
+                    if (!arena.countdownInitiatedHoldFire)
+                    {
+                        if (player.wantToJump > 0 && player.input[0].pckp && player.canJump <= 0 && !player.monkAscension && !player.tongue.Attached && player.bodyMode != Player.BodyModeIndex.Crawl && player.bodyMode != Player.BodyModeIndex.CorridorClimb && player.bodyMode != Player.BodyModeIndex.ClimbIntoShortCut && player.animation != Player.AnimationIndex.HangFromBeam && player.animation != Player.AnimationIndex.ClimbOnBeam && player.bodyMode != Player.BodyModeIndex.WallClimb && player.bodyMode != Player.BodyModeIndex.Swimming && player.Consious && !player.Stunned && player.animation != Player.AnimationIndex.AntlerClimb && player.animation != Player.AnimationIndex.VineGrab && player.animation != Player.AnimationIndex.ZeroGPoleGrab)
+                        {
+                            player.maxGodTime = arena.arenaSaintAscendanceTimer;
+                            player.ActivateAscension();
+                        }
+                        if (player.wantToJump > 0 && player.monkAscension)
+                        {
+                            player.DeactivateAscension();
+                        }
+                    }
+                //} else
+                //{
 
-            if (player.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Saint && !arena.countdownInitiatedHoldFire)
-            {
-                if (player.wantToJump > 0 && player.input[0].pckp && player.canJump <= 0 && !player.monkAscension && !player.tongue.Attached && player.bodyMode != Player.BodyModeIndex.Crawl && player.bodyMode != Player.BodyModeIndex.CorridorClimb && player.bodyMode != Player.BodyModeIndex.ClimbIntoShortCut && player.animation != Player.AnimationIndex.HangFromBeam && player.animation != Player.AnimationIndex.ClimbOnBeam && player.bodyMode != Player.BodyModeIndex.WallClimb && player.bodyMode != Player.BodyModeIndex.Swimming && player.Consious && !player.Stunned && player.animation != Player.AnimationIndex.AntlerClimb && player.animation != Player.AnimationIndex.VineGrab && player.animation != Player.AnimationIndex.ZeroGPoleGrab)
-                {
-                    player.maxGodTime = arena.arenaSaintAscendanceTimer;
-                    player.ActivateAscension();
-                }
-                if (player.wantToJump > 0 && player.monkAscension)
-                {
-                    player.DeactivateAscension();
-                }
-            }
+                //}
+            } 
+
 
             //if (player.SlugCatClass == SlugcatStats.Name.Night)
             //{
