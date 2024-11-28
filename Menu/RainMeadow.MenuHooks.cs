3e279d1a78c75c676dde640446796dfc45e3fc80
choc <notchoc@disroot.org>
2024-11-29 00:21:24 +0800

    story menu client start button ENTER

--- Menu/RainMeadow.MenuHooks.cs
+++ Menu/RainMeadow.MenuHooks.cs
@@ -25,6 +25,7 @@ namespace RainMeadow
 
             On.Menu.SlugcatSelectMenu.SlugcatUnlocked += SlugcatSelectMenu_SlugcatUnlocked;
             On.Menu.SlugcatSelectMenu.StartGame += SlugcatSelectMenu_StartGame;
+            On.Menu.SlugcatSelectMenu.UpdateStartButtonText += SlugcatSelectMenu_UpdateStartButtonText;
         }
 
         private bool SlugcatSelectMenu_SlugcatUnlocked(On.Menu.SlugcatSelectMenu.orig_SlugcatUnlocked orig, SlugcatSelectMenu self, SlugcatStats.Name i)
@@ -45,6 +46,19 @@ namespace RainMeadow
                 menu.PreStartGame(storyGameCharacter);
             }
             orig(self, storyGameCharacter);
+        }
+
+        private void SlugcatSelectMenu_UpdateStartButtonText(On.Menu.SlugcatSelectMenu.orig_UpdateStartButtonText orig, SlugcatSelectMenu self)
+        {
+            if (self is StoryOnlineMenu menu && OnlineManager.lobby != null && !OnlineManager.lobby.isOwner)
+            {
+                self.startButton.fillTime = 40f;
+                self.startButton.menuLabel.text = self.Translate("ENTER");
+            }
+            else
+            {
+                orig(self);
+            }
         }
 
         private void MenuScene_BuildScene(On.Menu.MenuScene.orig_BuildScene orig, MenuScene self)
