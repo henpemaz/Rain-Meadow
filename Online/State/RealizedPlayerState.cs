bd804e5994c5130893a4c134b78bfe3343c886d4
choc <notchoc@disroot.org>
2024-12-01 10:11:06 +0800

    refactor playerinantlers readto

--- Online/State/RealizedPlayerState.cs
+++ Online/State/RealizedPlayerState.cs
@@ -47,6 +47,19 @@ namespace RainMeadow
             }
         }
 
+        public void ReadTo(Deer.PlayerInAntlers playerInAntlers)
+        {
+            var deer = playerInAntlers.deer;
+            if (!deer.playersInAntlers.Contains(playerInAntlers))
+                deer.playersInAntlers.Add(playerInAntlers);
+            playerInAntlers.dangle = dangle;
+            if (playerInAntlers.stance is Deer.PlayerInAntlers.GrabStance stance)
+            {
+                stance.upper = upperAntlerPoint?.GetAntlerPoint();
+                stance.lower = lowerAntlerPoint?.GetAntlerPoint();
+            }
+        }
+
         public void ReadTo(Player player)
         {
             if (onlineDeer?.apo.realizedObject is not Deer deer) { RainMeadow.Error("deer not found: " + onlineDeer); return; }
@@ -56,14 +69,7 @@ namespace RainMeadow
                 player.playerInAntlers = null;
             }
             player.playerInAntlers ??= new Deer.PlayerInAntlers(player, deer);
-            if (!deer.playersInAntlers.Contains(player.playerInAntlers))
-                deer.playersInAntlers.Add(player.playerInAntlers);
-            player.playerInAntlers.dangle = dangle;
-            if (player.playerInAntlers.stance is Deer.PlayerInAntlers.GrabStance stance)
-            {
-                stance.upper = upperAntlerPoint?.GetAntlerPoint();
-                stance.lower = lowerAntlerPoint?.GetAntlerPoint();
-            }
+            this.ReadTo(player.playerInAntlers);
         }
     }
 
