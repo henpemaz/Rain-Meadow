1edbde56da4af7a0daf95eaea33ea44e6b8261b6
sixfears7 <57415489+6fears7@users.noreply.github.com>
2025-01-04 06:18:08 -0500

    fix parenthesis

--- Story/StoryHooks.cs
+++ Story/StoryHooks.cs
@@ -1351,32 +1351,33 @@ namespace RainMeadow
         private void VoidSeaScene_Update(On.VoidSea.VoidSeaScene.orig_Update orig, VoidSea.VoidSeaScene self, bool eu)
         {
             orig(self, eu);
-            if (isStoryMode(out _) {
-            foreach (var playerAvatar in OnlineManager.lobby.playerAvatars.Select(kv => kv.Value))
+            if (isStoryMode(out _))
             {
-                if (playerAvatar.type == (byte)OnlineEntity.EntityId.IdType.none) continue; // not in game
-                if (playerAvatar.FindEntity(true) is OnlinePhysicalObject opo && opo.apo is AbstractCreature ac)
+                foreach (var playerAvatar in OnlineManager.lobby.playerAvatars.Select(kv => kv.Value))
                 {
-                    // do things with the AbstractCreature we found
-                    if (!ac.IsLocal() && opo.apo.realizedObject.Submersion > 0.5f)
+                    if (playerAvatar.type == (byte)OnlineEntity.EntityId.IdType.none) continue; // not in game
+                    if (playerAvatar.FindEntity(true) is OnlinePhysicalObject opo && opo.apo is AbstractCreature ac)
                     {
-                        Vector2 position = ac.realizedCreature.bodyChunks[0].pos;
-                        RainMeadow.Debug("Removed onlinePlayer avatar on submersion at pos: " + position);
-                        opo.apo.realizedObject.room.AddObject(new ShockWave(position, 300f, 0.2f, 15, false));
-                        opo.apo.realizedObject.room.PlaySound(SoundID.MENU_Karma_Ladder_Hit_Upper_Cap, 0f, 3f, 1f);
-                        opo.apo.realizedObject.RemoveFromRoom();
-                    }
-                    else if (ac.IsLocal() && opo.apo.realizedObject.Submersion > 0.5f)
-                    {
-                        inVoidSea = true;
-                    }
-                    else if (ac.IsLocal() && !(opo.apo.realizedObject.Submersion > 0.5f))
-                    {
-                        inVoidSea = false;
+                        // do things with the AbstractCreature we found
+                        if (!ac.IsLocal() && opo.apo.realizedObject.Submersion > 0.5f)
+                        {
+                            Vector2 position = ac.realizedCreature.bodyChunks[0].pos;
+                            RainMeadow.Debug("Removed onlinePlayer avatar on submersion at pos: " + position);
+                            opo.apo.realizedObject.room.AddObject(new ShockWave(position, 300f, 0.2f, 15, false));
+                            opo.apo.realizedObject.room.PlaySound(SoundID.MENU_Karma_Ladder_Hit_Upper_Cap, 0f, 3f, 1f);
+                            opo.apo.realizedObject.RemoveFromRoom();
+                        }
+                        else if (ac.IsLocal() && opo.apo.realizedObject.Submersion > 0.5f)
+                        {
+                            inVoidSea = true;
+                        }
+                        else if (ac.IsLocal() && !(opo.apo.realizedObject.Submersion > 0.5f))
+                        {
+                            inVoidSea = false;
+                        }
                     }
                 }
             }
         }
-      }
     }
 }
