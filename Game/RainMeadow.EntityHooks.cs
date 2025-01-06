06cac472f0e55a743460855f7e6cb7d19d7cf2c4
6fears7 <6fears7even@gmail.com>
2025-01-06 17:25:40 -0500

    comments

--- Game/RainMeadow.EntityHooks.cs
+++ Game/RainMeadow.EntityHooks.cs
@@ -267,9 +267,9 @@ namespace RainMeadow
                             // if they're the overseer and it isn't the host moving it, that's bad as well
                             if (!opo.isMine || (apo is AbstractCreature ac && ac.creatureTemplate.type == CreatureTemplate.Type.Overseer && !newWorldSession.isOwner))
                             {
-                                if (opo.apo is AbstractCreature slug && slug.creatureTemplate.type.value == CreatureTemplate.Type.Slugcat.value) // test 
+                                if (opo.apo is AbstractCreature slug && slug.creatureTemplate.type.value == CreatureTemplate.Type.Slugcat.value)
                                 {
-                                    continue; // we will clean up later
+                                    continue; // we will clean up later after client rejoins
                                 }
                                 // not-online-aware removal
                                 Debug("removing remote entity from game " + opo);
