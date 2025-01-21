3af42e311a4dffefedbbc812e800f2ce775eecb4
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-22 02:20:15 +0300

    New object for chattextbox (rough skeleton)

---
+++ Menu/Objects/ChatButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Menu;
+
+namespace RainMeadow
+{
+    public abstract class ChatButton : ButtonTemplate
+    {
+        public HSLColor labelColor;
+
+        public MenuLabel menuLabel;
+
+        public RoundedRect roundedRect;
+
+        public ChatButton(Menu.Menu menu, MenuObject owner, string displayText, Vector2 pos, Vector2 size) : base(menu, owner, pos, size)
+        {
+            RainMeadow.Error("This is my parent constructor coming to life");
+            labelColor = Menu.Menu.MenuColor(Menu.Menu.MenuColors.White);
+            roundedRect = new RoundedRect(menu, owner, new Vector2(0f, 0f), size, true);
+            this.subObjects.Add(roundedRect);
+            menuLabel = new MenuLabel(menu, owner, displayText, new Vector2(-roundedRect.size.x / 2 + 10f, 0f), size, false);
+            menuLabel.label.alignment = FLabelAlignment.Left;
+            this.subObjects.Add(menuLabel);
+        }
+    }
+}
--- Menu/Objects/ChatTextBox.cs
+++ Menu/Objects/ChatTextBox.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 
 namespace RainMeadow
 {
-    public class ChatTextBox : SimpleButton, ICanBeTyped
+    public class ChatTextBox : ChatButton, ICanBeTyped
     {
         private SteamMatchmakingManager steamMatchmakingManager;
         private ButtonTypingHandler typingHandler;
@@ -20,7 +20,7 @@ namespace RainMeadow
         public Action<char> OnKeyDown { get; set; }
         public static int textLimit = 75;
         public static string lastSentMessage = "";
-        public ChatTextBox(Menu.Menu menu, MenuObject owner, string displayText, Vector2 pos, Vector2 size) : base(menu, owner, displayText, "", pos, size)
+        public ChatTextBox(Menu.Menu menu, MenuObject owner, string displayText, Vector2 pos, Vector2 size) : base(menu, owner, displayText, pos, size)
         {
             steamMatchmakingManager = MatchmakingManager.instance as SteamMatchmakingManager;
             lastSentMessage = "";
