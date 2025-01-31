9dba3a88c58757792c784c5df47eab3acae72a17
invalidunits <invalidunits@gmail.com>
2025-01-31 17:13:45 -0500

    LAN and Peer Changes

--- Online/Matchmaking/UdpPeer.cs
+++ Online/Matchmaking/UdpPeer.cs
@@ -655,12 +655,19 @@ namespace RainMeadow
         }
 
         public delegate void OnPeerForgotten_t(IPEndPoint endPoint);
+
+        public delegate bool ShouldForgetPeer_t(IPEndPoint endPoint);
+
+
+        public ShouldForgetPeer_t shouldForgetPeer = delegate { return true; };
         public event OnPeerForgotten_t OnPeerForgotten = delegate { };
         
         
         public void ForgetPeer(RemotePeer peer) {
-            OnPeerForgotten.Invoke(peer.PeerEndPoint);
-            peers.Remove(peer);
+            if (shouldForgetPeer(peer.PeerEndPoint)) {
+                OnPeerForgotten.Invoke(peer.PeerEndPoint);
+                peers.Remove(peer);
+            }
         }
         public void ForgetPeer(IPEndPoint endPoint) {
             var remove_peers = peers.FindAll(x => CompareIPEndpoints(endPoint, x.PeerEndPoint));
