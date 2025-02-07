8e4d93d56a8800e1e0c4c69248bfe9c1e1f62631
UO <57415489+6fears7@users.noreply.github.com>
2025-02-07 15:21:46 -0500

    Fix indefinite tinnitus

--- Game/RainMeadow.GameplayHooks.cs
+++ Game/RainMeadow.GameplayHooks.cs
@@ -1,4 +1,3 @@
-﻿using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using MonoMod.Utils;
 using System;
@@ -37,6 +36,20 @@ namespace RainMeadow
             On.RoomRealizer.Update += RoomRealizer_Update;
             On.Creature.Die += Creature_Die; // do not die!
             IL.Player.TerrainImpact += Player_TerrainImpact;
+            On.DeafLoopHolder.Update += DeafLoopHolder_Update;
+        }
+
+        private void DeafLoopHolder_Update(On.DeafLoopHolder.orig_Update orig, DeafLoopHolder self, bool eu)
+        {
+            orig(self, eu);
+            if (OnlineManager.lobby != null)
+            {
+                if (self.player != null && self.player.dead && self.deafLoop != null)
+                {
+                    self.deafLoop = null;
+                }
+            }
+
         }
 
         private void Player_TerrainImpact(ILContext il)
@@ -432,7 +445,6 @@ namespace RainMeadow
                         RainMeadow.Debug("prevent abstract creature destroy: " + self); // need this so that we don't release the world session on death
                         self.Die();
                         self.State.alive = false;
-
                     }
                 }
             }
