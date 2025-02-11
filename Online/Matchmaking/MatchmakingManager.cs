df6bcdec95058a35d7bc65476f0a9e075f86f98a
Henrique Maziero <henpemaz@gmail.com>
2025-02-11 22:45:25 +0100

    version bump

--- Online/Matchmaking/MatchmakingManager.cs
+++ Online/Matchmaking/MatchmakingManager.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using Unity.Services.Analytics.Internal;
 
 namespace RainMeadow
 {
