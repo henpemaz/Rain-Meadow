2ec3161c326a04a01fe7687f9a92f5ea64f5715b
forthfora <forthfora@gmail.com>
2025-02-11 19:09:45 +0800

    Revert "Prevented me from building...?"
    This reverts commit f3a3c453214a2c8fa5efeaccbd2edaf8c01c8791.

--- Online/Matchmaking/MatchmakingManager.cs
+++ Online/Matchmaking/MatchmakingManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Unity.Services.Analytics.Internal;
 
 namespace RainMeadow
 {
