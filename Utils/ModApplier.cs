3b1c576fed3748c069cdbea4879f6d0dbd8b64ba
TheLazyCowboy1 <Nathan@331oak.com>
2025-02-20 14:58:52 -0500

    Re-adding mod applier code

--- Utils/ModApplier.cs
+++ Utils/ModApplier.cs
@@ -21,6 +21,12 @@ namespace RainMeadow
             menu = (Menu.Menu)manager.currentMainLoop;
         }
 
+        private void Cancel()
+        {
+            On.RainWorld.Update -= RainWorld_Update;
+            this.finished = true;
+        }
+
         private void RainWorld_Update(On.RainWorld.orig_Update orig, RainWorld self)
         {
             orig(self);
@@ -36,8 +42,10 @@ namespace RainMeadow
 
             if (IsFinished())
             {
-                On.RainWorld.Update -= RainWorld_Update;
+                Cancel();
 
+                //go to main menu (to finish applying changes) and then rejoin the lobby
+                manager.dialog = null;
                 manager.rainWorld.options.Save();
                 if (this.applyError != null)
                 {
@@ -72,6 +80,8 @@ namespace RainMeadow
                 modMismatchString += Environment.NewLine + menu.Translate("Mods that have to be disabled: ") + string.Join(", ", modsToDisable.ConvertAll(mod => mod.LocalizedName));
             if (unknownMods.Count > 0)
                 modMismatchString += Environment.NewLine + menu.Translate("Mods that have to be installed: ") + string.Join(", ", unknownMods);
+            else
+                modMismatchString += Environment.NewLine + Environment.NewLine + menu.Translate("Apply these changes now?");
 
             // modMismatchString += Environment.NewLine + Environment.NewLine + menu.Translate("You will be returned to the Lobby Select screen");
 
@@ -90,17 +100,70 @@ namespace RainMeadow
                 checkUserConfirmation = null;
                 OnlineManager.LeaveLobby();
                 manager.RequestMainProcessSwitch(RainMeadow.Ext_ProcessID.LobbySelectMenu);
+                Cancel();
             };
 
             // disable auto-apply for now
-            //if (unknownMods.Any())
-            //{
-            //    checkUserConfirmation = new DialogNotify(modMismatchString, new Vector2(480f, 320f), manager, cancelProceed);
-            //}
-            //else
-            //{
-            //    checkUserConfirmation = new DialogConfirm(modMismatchString, new Vector2(480f, 320f), manager, confirmProceed, cancelProceed);
-            //}
+            //nah that seems like a really useful feature I'mma re-add it - TheLazyCowboy1
+            if (unknownMods.Count > 0)
+            {
+                checkUserConfirmation = new DialogNotify(modMismatchString, new Vector2(480f, 320f), manager, cancelProceed);
+            }
+            else
+            {
+                checkUserConfirmation = new DialogConfirm(modMismatchString, new Vector2(480f, 320f), manager, confirmProceed, cancelProceed);
+            }
+            //checkUserConfirmation = new DialogNotify(modMismatchString, new Vector2(480f, 320f), manager, cancelProceed);
+
+            manager.ShowDialog(checkUserConfirmation);
+        }
+
+        public void ConfirmReorder()
+        {
+            var modMismatchString = menu.Translate("Warning: Differing Mod Load Orders!")
+                + Environment.NewLine + menu.Translate("This may cause unstable play.")
+                + Environment.NewLine + Environment.NewLine + menu.Translate("Reorder your mods now?");
+
+            Action confirmProceed = () =>
+            {
+                manager.dialog = null;
+                checkUserConfirmation = null;
+                manager.RequestMainProcessSwitch(RainMeadow.Ext_ProcessID.LobbySelectMenu);
+                OnlineManager.LeaveLobby();
+                dialogBox = new DialogAsyncWait(menu, menu.Translate("mod_menu_apply_mods"), new Vector2(480f, 320f));
+                manager.ShowDialog(dialogBox);
+                Start(filesInBadState);
+            };
+
+            Action cancelProceed = () =>
+            {
+                manager.dialog = null;
+                checkUserConfirmation = null;
+                Cancel();
+            };
+
+            // disable auto-apply for now
+            //nah that seems like a really useful feature I'mma re-add it - TheLazyCowboy1
+            checkUserConfirmation = new DialogConfirm(modMismatchString, new Vector2(480f, 320f), manager, confirmProceed, cancelProceed);
+
+            manager.ShowDialog(checkUserConfirmation);
+        }
+
+        public void ShowMissingDLCMessage(List<ModManager.Mod> missingDLC)
+        {
+            var modMismatchString = menu.Translate("Cannot join due to missing DLC!") + Environment.NewLine;
+
+            modMismatchString += Environment.NewLine + menu.Translate("Missing DLC Mods that have to be enabled: ") + string.Join(", ", missingDLC.ConvertAll(mod => mod.LocalizedName));
+
+            Action cancelProceed = () =>
+            {
+                manager.dialog = null;
+                checkUserConfirmation = null;
+                OnlineManager.LeaveLobby();
+                manager.RequestMainProcessSwitch(RainMeadow.Ext_ProcessID.LobbySelectMenu);
+                Cancel();
+            };
+
             checkUserConfirmation = new DialogNotify(modMismatchString, new Vector2(480f, 320f), manager, cancelProceed);
 
             manager.ShowDialog(checkUserConfirmation);
