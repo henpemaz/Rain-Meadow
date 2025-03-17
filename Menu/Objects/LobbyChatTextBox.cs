8e6fe3a0af1bc7be0f6540a81d0d6fbac548d4ee
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-03-17 05:28:55 +0300

    Small initial work on lobby chat
    wanted to work on this yesterday but i was sick

---
+++ Menu/Objects/LobbyChatTextBox.cs
@@ -0,0 +1,13 @@
+﻿using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace RainMeadow
+{
+    public class LobbyChatTextBox : OpTextBox
+    {
+        public LobbyChatTextBox(ConfigurableBase config, Vector2 pos, float sizeX) : base(config, pos, sizeX)
+        {
+            this.maxLength = (int)Mathf.Round(sizeX / 5);
+        }
+    }
+}
