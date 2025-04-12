497129ed531eed7c3601ef74d3c1a5748cbdd553
Dustylemny <hebengcn@gmail.com>
2025-04-12 16:07:19 +0800

    Implementation of player scroll buttons

--- OnlineUIComponents/SpectatorOverlay.cs
+++ OnlineUIComponents/SpectatorOverlay.cs
@@ -7,174 +7,206 @@ namespace RainMeadow
 {
     public class SpectatorOverlay : Menu.Menu
     {
-        public AbstractCreature? spectatee;
-
-        public Vector2 pos;
-
-        public class PlayerButton
-        {
-            public OnlinePlayer player;
-            public OnlinePhysicalObject opo;
-            public SimplerButton button;
-            public SimplerSymbolButton? kickbutton;
-            public bool mutePlayer
-            {
-                get => OnlineManager.lobby.gameMode.mutedPlayers.Contains(player.id.name);
-                set
-                {
-                    var name = player.id.name;
-                    if (value)
-                    {
-                        RainMeadow.Debug($"Added {name} to mute list");
-                        OnlineManager.lobby.gameMode.mutedPlayers.Add(name);
-                    }
-                    else
-                    {
-                        RainMeadow.Debug($"Removed {name} from mute list");
-                        OnlineManager.lobby.gameMode.mutedPlayers.Remove(name);
-                    }
-                }
-            }
-            private string clientMuteSymbol => mutePlayer ? "FriendA" : "Menu_Symbol_Clear_All";
-            public Vector2 pos
-            {
-                set
-                {
-                    button.pos = value;
-                    if (kickbutton != null)
-                        kickbutton.pos = value + new Vector2(120, 0);
-                }
-            }
-            public SpectatorOverlay overlay;
-
-            public PlayerButton(SpectatorOverlay menu, OnlinePlayer player, OnlinePhysicalObject? opo, Vector2 pos, bool canKick = false)
-            {
-                this.overlay = menu;
-                this.player = player;
-                this.opo = opo ?? null;
-                this.button = new SimplerButton(menu, menu.pages[0], player.id.name, pos, new Vector2(110, 30));
-
-                this.button.OnClick += (_) =>
-                {
-                    this.button.toggled ^= true;
-                    overlay.spectatee = this.button.toggled ? opo?.apo as AbstractCreature : null;
-                };
-                this.button.owner.subObjects.Add(button);
-                if (canKick)
-                {
-                    this.kickbutton = new SimplerSymbolButton(menu, menu.pages[0], "Menu_Symbol_Clear_All", "KICKPLAYER", pos + new Vector2(120, 0));
-                    this.kickbutton.OnClick += (_) => BanHammer.BanUser(player);
-                    this.kickbutton.owner.subObjects.Add(kickbutton);
-                }
-                if (player != OnlineManager.mePlayer)
-                {
-                    this.kickbutton = new SimplerSymbolButton(menu, menu.pages[0], clientMuteSymbol, "MUTEPLAYER", pos + new Vector2(120, 0));
-                    this.kickbutton.OnClick += (_) =>
-                    {
-                        mutePlayer ^= true;
-                        this.kickbutton.UpdateSymbol(clientMuteSymbol);
-                    };
-                    this.kickbutton.owner.subObjects.Add(kickbutton);
-                }
-                this.pos = pos;
-            }
-
-            public void Destroy()
-            {
-                this.button.RemoveSprites();
-                this.button.page.RemoveSubObject(this.button);
-                if (this.kickbutton != null)
-                {
-                    this.kickbutton.RemoveSprites();
-                    this.kickbutton.page.RemoveSubObject(this.kickbutton);
-                }
-            }
-        }
-
-        public RainWorldGame game;
-        public List<PlayerButton> playerButtons;
-
+        public static int MaxVisibleOnList => 8;
+        public static float ButtonSpacingOffset => 8;
+        public static float ButtonSize => 30;
+        public List<PlayerButton> PlayerButtons => playerScroller.GetSpecificButtons<PlayerButton>();
         public SpectatorOverlay(ProcessManager manager, RainWorldGame game) : base(manager, RainMeadow.Ext_ProcessID.SpectatorMode)
         {
             this.game = game;
-            this.pages.Add(new Page(this, null, "spectator", 0));
-            this.selectedObject = null;
-            this.playerButtons = new();
-            this.pos = new Vector2(1180, 553);
-            this.pages[0].subObjects.Add(new Menu.MenuLabel(this, this.pages[0], this.Translate("PLAYERS"), this.pos, new(110, 30), true));
+            pages.Add(new Page(this, null, "spectator", 0));
+            selectedObject = null;
+            pos = new Vector2(1180, 553);
+            pages[0].subObjects.Add(new Menu.MenuLabel(this, this.pages[0], this.Translate("PLAYERS"), this.pos, new(110, 30), true));
+            playerScroller = new(this, pages[0], new(pos.x, pos.y - 38 - ButtonScroller.CalculateHeightBasedOnAmtOfButtons(MaxVisibleOnList, ButtonSize, ButtonSpacingOffset)), MaxVisibleOnList, 200, ButtonSize, ButtonSpacingOffset);
+            pages[0].subObjects.Add(playerScroller);
+            playerScroller.ConstrainScroll();
         }
 
         private bool UpdateList()
         {
-            List<OnlinePlayer> newPlayers = OnlineManager.players
-                .OrderBy(onlineP => onlineP.isMe ? 0 : 1)
-                .ToList(); // will keep this logic for LAN
-
-            List<OnlinePhysicalObject> realizedPlayers = OnlineManager.lobby.playerAvatars
-                .Select(kv => kv.Value.FindEntity(true))
-                .OfType<OnlinePhysicalObject>()
-                .OrderBy(opo => opo.isMine ? 0 : 1)
-                .ToList();
-
+            List<PlayerButton> playerButtons = PlayerButtons;
+            List<OnlinePlayer> newPlayers = [..OnlineManager.players.OrderBy(onlineP => onlineP.isMe ? 0 : 1)]; // will keep this logic for LAN
+            List<OnlinePhysicalObject> realizedPlayers = [.. OnlineManager.lobby.playerAvatars.Select(kv => kv.Value.FindEntity(true)).OfType<OnlinePhysicalObject>().OrderBy(opo => opo.isMine ? 0 : 1)];
             if (newPlayers.Count == playerButtons.Count) return false; // race condition that will Never Happen(TM)
 
-            var offset = new Vector2(0, 38);
-            var pos = this.pos - offset;
-
-            for (var i = playerButtons.Count - 1; i >= 0; i--)
+            for (int i = playerButtons.Count - 1; i >= 0; i--)
             {
-                var button = playerButtons[i];
+                PlayerButton button = playerButtons[i];
                 if (newPlayers.Contains(button.player))
                 {
                     newPlayers.Remove(button.player);
-                    button.pos = pos;
-                    pos -= offset;
+                    continue;
                 }
-                else
-                {
-                    button.Destroy();
-                    playerButtons.RemoveAt(i);
-                }
+                playerScroller.RemoveButton(i, false);
             }
-
-            foreach (var player in newPlayers)
+            foreach (OnlinePlayer player in newPlayers)
             {
-                var foundPlayer = realizedPlayers.FirstOrDefault(x => x.owner == player);
-                if (foundPlayer == null) // player joined but is unrealized, null them out
-                {
-                    playerButtons.Add(new PlayerButton(this, player, null, pos, OnlineManager.lobby.isOwner && !player.isMe));
-                }
-                else
-                {
-                    playerButtons.Add(new PlayerButton(this, player, foundPlayer, pos, OnlineManager.lobby.isOwner && !foundPlayer.isMine));
-
-                }
-                pos -= offset;
-
+                OnlinePhysicalObject? foundPlayer = realizedPlayers.FirstOrDefault(x => x.owner == player);
+                PlayerButton button = foundPlayer == null? new(this, playerScroller, player, null, OnlineManager.lobby.isOwner && !player.isMe) : new(this, playerScroller, player, foundPlayer, OnlineManager.lobby.isOwner && !foundPlayer.isMine);
+                playerScroller.AddScrollObjects(button);
             }
-
+            playerScroller.ConstrainScroll();
             return true;
         }
-
         public override void Update()
         {
             base.Update();
             UpdateList();
-
-            foreach (var button in playerButtons)
+            foreach (PlayerButton button in PlayerButtons)
             {
-                AbstractCreature? ac;
-                if (button.opo != null)
-                {
-                    ac = button.opo.apo as AbstractCreature;
-                }
-                else
-                {
-                    ac = null;
-                }
-                button.button.toggled = ac != null && ac == spectatee;
-                button.button.buttonBehav.greyedOut = ac is null || (ac.state.dead || (ac.realizedCreature != null && ac.realizedCreature.State.dead));
+                AbstractCreature? ac = button.opo?.apo as AbstractCreature;
+                button.toggled = ac != null && ac == spectatee;
+                button.buttonBehav.greyedOut = ac is null || (ac.state.dead || (ac.realizedCreature != null && ac.realizedCreature.State.dead));
             }
+        }
+        public override string UpdateInfoText()
+        {
+            return GetIHaveDescriptionObject(selectedObject)?.Description ?? base.UpdateInfoText();
+        }
+        public IHaveADescription? GetIHaveDescriptionObject(MenuObject obj)
+        {
+            MenuObject menuObj = obj;
+            while (menuObj != null && menuObj is not IHaveADescription)
+            {
+                menuObj = menuObj.owner;
+            }
+            return menuObj as IHaveADescription;
+        }
+        public void SpectatePlayer(AbstractCreature? aC)
+        {
+            spectatee = aC;
+        }
+        public AbstractCreature? spectatee;
+        public Vector2 pos;
+        public RainWorldGame game;
+        public ButtonScroller playerScroller;
+        public class PlayerButton : ButtonScroller.ScrollerButton
+        {
+            public bool BanClickedOnce
+            {
+                get
+                {
+                    return banClickedOnce;
+                }
+                set
+                {
+                    if (value != banClickedOnce)
+                    {
+                        HoldKick = true;
+                        banClickedOnce = value;
+                        labelColor = banClickedOnce ? MenuColor(MenuColors.DarkRed) : MenuColor(MenuColors.MediumGrey);
+                        Description = banClickedOnce ? "Press again to ban player! Select another button if you dont want to!" : "";
+                        menu.infolabelDirty = true;
+                        HoldKick = false;
+                    }
+                }
+            }
+            public bool HoldKick
+            {
+                set
+                {
+                    if (kickbutton != null)
+                    {
+                        kickbutton.buttonBehav.greyedOut = value;
+                    }
+                }
+            }
+            public bool MutePlayer
+            {
+                get => OnlineManager.lobby.gameMode.mutedPlayers.Contains(player.id.name);
+                set
+                {
+                    string name = player.id.name;
+                    if (value != MutePlayer)
+                    {
+                        if (value)
+                        {
+                            RainMeadow.Debug($"Added {name} to mute list");
+                            OnlineManager.lobby.gameMode.mutedPlayers.Add(name);
+                        }
+                        else
+                        {
+                            RainMeadow.Debug($"Removed {name} from mute list");
+                            OnlineManager.lobby.gameMode.mutedPlayers.Remove(name);
+                        }
+                        menu.PlaySound(value? SoundID.MENU_Checkbox_Check : SoundID.MENU_Checkbox_Uncheck);
+                        kickbutton?.UpdateSymbol(ClientMuteSymbol);
+                    }
+                }
+            }
+            private string ClientMuteSymbol => MutePlayer ? "FriendA" : "Menu_Symbol_Clear_All";
+            public SpectatorOverlay? Overlay
+            {
+                get => menu as SpectatorOverlay;
+            }
+            public PlayerButton(SpectatorOverlay menu, MenuObject owner, OnlinePlayer player, OnlinePhysicalObject? opo, bool canKick = false, Vector2 size = default) : base(menu, owner, player.id.name, Vector2.zero, size == default ? new(110, 30) : size)
+            {
+                this.player = player;
+                this.opo = opo;
+                OnClick += (_) =>
+                {
+                    toggled ^= true;
+                    Overlay?.SpectatePlayer(toggled ? this.opo?.apo as AbstractCreature : null);
+                };
+                if (canKick)
+                {
+                    kickbutton = new(menu, this, "Menu_Symbol_Clear_All", "KICKPLAYER", new(this.size.x + 10, 0));
+                    kickbutton.OnClick += (_) =>
+                    {
+                        if (BanClickedOnce)
+                        {
+                            BanHammer.BanUser(player);
+                            menu.PlaySound(SoundID.MENU_Remove_Level);
+                        }
+                        else
+                        {
+                            BanClickedOnce = true;
+                            menu.PlaySound(SoundID.MENU_Greyed_Out_Button_Clicked);
+                        }
+                    };
+                    subObjects.Add(kickbutton);
+                }
+                if (kickbutton == null && player != OnlineManager.mePlayer)  //prevent double sprites and double updating
+                {
+                    kickbutton = new(menu, this, ClientMuteSymbol, "MUTEPLAYER", new(this.size.x + 10, 0));
+                    kickbutton.OnClick += (_) =>
+                    {
+                        MutePlayer ^= true;
+                    };
+                    subObjects.Add(kickbutton);
+                }
+                menu.TryMutualBind(this, kickbutton, true);
+            }
+            public override void RemoveSprites()
+            {
+                base.RemoveSprites();
+                this.ClearMenuObject(ref kickbutton);
+            }
+            public override void UpdateAlpha(float alpha)
+            {
+                base.UpdateAlpha(alpha);
+                if (kickbutton != null)
+                {
+                    kickbutton.symbolSprite.alpha = alpha;
+                    for (int i = 0; i < kickbutton.roundedRect.sprites.Length; i++)
+                    {
+                        kickbutton.roundedRect.sprites[i].alpha = alpha;
+                        kickbutton.roundedRect.fillAlpha = alpha / 2;
+                    }
+                    kickbutton.GetButtonBehavior.greyedOut = alpha < 1;
+                }
+            }
+            public override void Update()
+            {
+                base.Update();
+                BanClickedOnce = menu.selectedObject == kickbutton && BanClickedOnce;
+            }
+
+            public bool banClickedOnce;
+            public OnlinePlayer player;
+            public OnlinePhysicalObject? opo;
+            public SimplerSymbolButton? kickbutton;
         }
     }
 }
