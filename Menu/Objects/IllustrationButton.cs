cff3b9fbb029376a7ec04c7af51ee92714cbc086
Dustylemny <hebengcn@gmail.com>
2025-05-04 10:51:37 +0800

    Adjustments
    Make Atlas cache once. Added portrait black to illustrationButton

--- Menu/Objects/IllustrationButton.cs
+++ Menu/Objects/IllustrationButton.cs
@@ -2,9 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RWCustom;
 using UnityEngine;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RainMeadow.UI.Components
 {
@@ -15,15 +14,33 @@ namespace RainMeadow.UI.Components
         public Vector2 Size { get => size; set => size = value; }
         public IllustrationButton(Menu.Menu menu, MenuObject owner, Vector2 pos, string folderName, string fileName) : base(menu, owner, pos, Vector2.zero)
         {
-            roundedRect = new(menu, this, Vector2.zero, size, true);
-            selectRect = new(menu, this, Vector2.zero, size, false);
             portrait = new(menu, this, folderName, fileName, Vector2.zero, true, true);
             size = portrait.size;
+            portrait.pos = size / 2;
+            roundedRect = new(menu, this, Vector2.zero, size, true);
+            selectRect = new(menu, this, Vector2.zero, size, false);
+            portrait.sprite.MoveInFrontOfOtherNode(selectRect.sprites.Last());
             subObjects.AddRange([portrait, roundedRect, selectRect]);
+        }
+        public override Color InterpColor(float timeStacker, HSLColor baseColor)
+        {
+            if (!buttonBehav.greyedOut && !inactive && !borderIgnorePortraitBlack)
+            {
+                float a = Mathf.Max(Mathf.Lerp(buttonBehav.lastCol, buttonBehav.col, timeStacker), Mathf.Lerp(buttonBehav.lastFlash, buttonBehav.flash, timeStacker));
+                HSLColor from = HSLColor.Lerp(baseColor, Menu.Menu.MenuColor(Menu.Menu.MenuColors.DarkGrey), Mathf.Lerp(lastPortraitBlack, portraitBlack, timeStacker));
+                from = HSLColor.Lerp(from, Menu.Menu.MenuColor(Menu.Menu.MenuColors.White), a);
+                return HSLColor.Lerp(from, Menu.Menu.MenuColor(Menu.Menu.MenuColors.Black), black).rgb;
+            }
+            return base.InterpColor(timeStacker, baseColor);
+        }
+        public virtual Color MyPortraitColor(Color? color,float timeStacker)
+        {
+            return Color.Lerp(color ?? Color.white, Color.black, Custom.SCurve(Mathf.Lerp(lastPortraitBlack, portraitBlack, timeStacker), 0.5f) * 0.75f);
         }
         public override void Update()
         {
             base.Update();
+            lastPortraitBlack = portraitBlack;
             buttonBehav.Update();
             roundedRect.fillAlpha = Mathf.Lerp(0.3f, 0.6f, buttonBehav.col);
             roundedRect.addSize = new Vector2(10f, 6f) * (buttonBehav.sizeBump + 0.5f * Mathf.Sin(buttonBehav.extraSizeBump * 3.1415927f)) * (buttonBehav.clicked ? 0f : 1f);
@@ -36,6 +53,7 @@ namespace RainMeadow.UI.Components
             roundedRect.size = size;
             selectRect.size = size;
             portrait.pos = size / 2;
+            portrait.color = MyPortraitColor(portraitColor, timeStacker);
 
         }
         public virtual void UpdateAlpha(float alpha)
@@ -59,8 +77,9 @@ namespace RainMeadow.UI.Components
             portrait.sprite.SetElementByName(portrait.fileName);
         }
 
-        public float alpha = 1, desiredOrigAlpha = 1;
-        public bool forceGreyedOut;
+        public float alpha = 1, desiredOrigAlpha = 1, portraitBlack = 1, lastPortraitBlack = 1;
+        public bool forceGreyedOut, borderIgnorePortraitBlack;
+        public Color? portraitColor;
         public MenuIllustration portrait;
         public RoundedRect roundedRect, selectRect;
     }
