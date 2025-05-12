59389da884672f58ae4bc3d1b1605f502f284d06
6fears7 <6fears7even@gmail.com>
2025-05-12 06:56:34 -0400

    fix room view

--- Game/RainMeadow.GameHooks.cs
+++ Game/RainMeadow.GameHooks.cs
@@ -52,26 +52,6 @@ namespace RainMeadow
 
             // Arena specific
             On.GameSession.AddPlayer += GameSession_AddPlayer;
-            On.Room.NowViewed += Room_NowViewed;
-
-        }
-
-        private void Room_NowViewed(On.Room.orig_NowViewed orig, Room self)
-        {
-            if (OnlineManager.lobby != null && ModManager.Watcher)
-            {
-                if (self.game.cameras[0].rippleData != null && self.game.cameras[0].lastRippleState == false && self.game.cameras[0].followAbstractCreature.GetOnlineCreature().isMine && self.game.cameras[0].followAbstractCreature.realizedCreature != null && self.game.cameras[0].followAbstractCreature.realizedCreature is Player pl && !pl.isCamo)
-                {
-                    self.game.cameras[0].rippleData.RemoveCommandBuffer();
-                    self.game.cameras[0].rippleData = null;
-                    if (self.fsRipple != null)
-                    {
-                        self.fsRipple.Destroy();
-                        self.fsRipple = null;
-                    }
-                }
-            }
-            orig(self);
         }
 
 
