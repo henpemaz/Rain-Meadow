f135ff49378f4ca6f82a630ff62db755bdcaf894
Timbits <timbits1123@gmail.com>
2025-05-18 10:20:19 -0400

    fix: Scroll Buttons on VerticalScrollSelector had fixed size
    also moved VerticalSlider patch

--- Menu/Components/Patched/VerticalSlider.cs
+++ Menu/Components/Patched/VerticalSlider.cs
@@ -1,12 +1,12 @@
 using Menu;
 using UnityEngine;
 
-namespace RainMeadow.UI.Components;
+namespace RainMeadow.UI.Components.Patched;
 
 /// <summary>
 /// when the slider's position is shifted on the x-axis, the lines will now not spasm. Moving the slider's position on the y-axis still has this issue, but why would you ever move the position of a slider on the y-axis?
 /// </summary>
-public class VerticalSliderPlus(Menu.Menu menu, MenuObject owner, string? text, Vector2 pos, Vector2 size, Slider.SliderID ID, bool subtleSlider)
+public class PatchedVerticalSlider(Menu.Menu menu, MenuObject owner, string? text, Vector2 pos, Vector2 size, Slider.SliderID ID, bool subtleSlider)
     : VerticalSlider(menu, owner, text, pos, size, ID, subtleSlider)
 {
     public override void GrafUpdate(float timeStacker)
