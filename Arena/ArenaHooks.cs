b9353760a4ece3002f381a7e70dd86ca4dc005b7
UO <57415489+6fears7@users.noreply.github.com>
2025-05-30 13:44:49 -0400

    Update ArenaHook to properly exit

--- Arena/ArenaHooks.cs
+++ Arena/ArenaHooks.cs
@@ -542,7 +542,7 @@ namespace RainMeadow
 
         private void PauseMenu_Singal(On.Menu.PauseMenu.orig_Singal orig, Menu.PauseMenu self, Menu.MenuObject sender, string message)
         {
-            if (message == "QUIT" && isArenaMode(out var arena))
+            if (message == "EXIT" && isArenaMode(out var arena))
             {
                 if (OnlineManager.lobby.isOwner)
                 {
