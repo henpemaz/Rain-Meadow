bf6cd6eed0cddf37b41c3d8dc16783a4e053c05e
Noah Hewitt <52514366+invalidunits@users.noreply.github.com>
2025-07-11 15:07:41 -0400

    Jolly Coop Support (#865)
    * begining of a journey
    
    * Support multiple Avatars
    
    * Add some Jolly good hooks
    
    * UI and Some Starving Progress?
    
    * More Hud Stuff Yay
    
    * Stop remote from changing camera
    
    * Fixed colors
    
    * Jolly Player Icons (still a bit broken)
    
    * More Camera Stuff
    
    * Fix Pup grabbing
    
    * Story Check
    
    * Don't use remix color buttons for Jolly
    
    * Fixed Survivor and Hunter Intro Issues
    
    * Wake Up Sleepyhead
    
    * Food Changes
    
    * begining of a journey
    
    * Support multiple Avatars
    
    * Add some Jolly good hooks
    
    * UI and Some Starving Progress?
    
    * More Hud Stuff Yay
    
    * Stop remote from changing camera
    
    * Fixed colors
    
    * Jolly Player Icons (still a bit broken)
    
    * More Camera Stuff
    
    * Fix Pup grabbing
    
    * Story Check
    
    * Don't use remix color buttons for Jolly
    
    * Fixed Survivor and Hunter Intro Issues
    
    * Food Synchronization Jolly Co-OP
    
    * Refactor player food management and update multiplayer avatar activation logic
    
    * Fix infinite recursion
    
    * Fix rebase error
    
    * Fixed infinite recursion for real
    
    * Fixed Malnourishment
    
    * Update malnourishment
    
    ---------
    
    Co-authored-by: UO <57415489+6fears7@users.noreply.github.com>

--- Online/State/RealizedPlayerState.cs
+++ Online/State/RealizedPlayerState.cs
@@ -94,6 +94,9 @@ namespace RainMeadow
 
     public class RealizedPlayerState : RealizedCreatureState
     {
+        [OnlineField]
+        public bool malnourished = false;
+
         [OnlineField(nullable = true)]
         private VinePositionState? vinePosState;
         [OnlineField(nullable = true)]
@@ -204,6 +207,8 @@ namespace RainMeadow
                 if (handIndex >= 0 && playerGraphics.hands[handIndex].reachingForObject)
                     pointingDir = playerGraphics.hands[handIndex].absoluteHuntPos;
             }
+
+            malnourished = p.Malnourished;
         }
 
         public Player.InputPackage GetInput()
@@ -337,6 +342,10 @@ namespace RainMeadow
                     playerGraphics.hands[handIndex].absoluteHuntPos = pointingDir.Value;
                     p.handPointing = handIndex; //important! - check != -1 so we know we are in a "pointing state"
                 }
+            }
+
+            if (p.Malnourished != malnourished) {
+                p.SetMalnourished(malnourished);
             }
         }
     }
