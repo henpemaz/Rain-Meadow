6e3a686f271d449de4a436158d44186837dd9ca8
6fears7 <57415489+6fears7@users.noreply.github.com>
2025-08-27 06:00:02 -0400

    Update StoryOnlineMenu.cs
    cannot read

--- Story/StoryOnlineMenu.cs
+++ Story/StoryOnlineMenu.cs
@@ -138,7 +138,7 @@ namespace RainMeadow
 
         public void SetSelectedSlugcat(int player, SlugcatStats.Name slugcat)
         {
-            if ((currentCampaign != slugcat && playerSelectedSlugcats[player] != null) || (playerSelectedSlugcats[player] == null && currentCampaign != slugcat))
+            if ((playerSelectedSlugcats[player] != slugcat && playerSelectedSlugcats[player] != null) || (playerSelectedSlugcats[player] == null && currentCampaign != slugcat))
             {
                 if (ModManager.JollyCoop)
                 {
@@ -692,5 +692,6 @@ namespace RainMeadow
         }
     }
 }
+
 
 
