6254efaecf57959a6a8c87b9e5eb18ae13b55bda
6fears7 <6fears7even@gmail.com>
2025-09-26 14:20:06 -0400

    does not work

--- Game/RainMeadow.LoadingHooks.cs
+++ Game/RainMeadow.LoadingHooks.cs
@@ -19,35 +19,8 @@ namespace RainMeadow
             On.RoomPreparer.ctor += RoomPreparer_ctor;
             On.AbstractRoom.Abstractize += AbstractRoom_Abstractize;
             On.ArenaSitting.NextLevel += ArenaSitting_NextLevel;
-            On.RoomCamera.ChangeRoom += RoomCamera_ChangeRoom;
-
             // new Hook(typeof(RainWorldGame).GetProperty(nameof(RainWorldGame.StoryCharacter)).GetGetMethod(), RainWorldGame_StoryCharacter);
             // new Hook(typeof(RainWorldGame).GetProperty(nameof(RainWorldGame.TimelinePoint)).GetGetMethod(), RainWorldGame_TimelinePoint);
-        }
-
-        private void RoomCamera_ChangeRoom(On.RoomCamera.orig_ChangeRoom orig, RoomCamera self, Room newRoom, int cameraPosition)
-        {
-            if (OnlineManager.lobby != null)
-            {
-                if (self.waterLight == null && newRoom.water)
-                {
-                    self.waterLight = new WaterLight(self, newRoom.game.rainWorld.Shaders["WaterLight"]);
-                }
-                else if (self.waterLight != null && !newRoom.water)
-                {
-                    self.waterLight.CleanOut();
-                    self.waterLight = null;
-                }
-
-                if (self.waterLight != null)
-                {
-                    if (newRoom.waterObject == null && newRoom.water)
-                    {
-                        newRoom.AddWater();
-                    }
-                }
-            }
-            orig(self, newRoom, cameraPosition);
         }
 
         SlugcatStats.Name RainWorldGame_StoryCharacter(Func<RainWorldGame, SlugcatStats.Name> orig, RainWorldGame self)
