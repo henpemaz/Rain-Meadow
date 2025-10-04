d00c5db28c9f1d0928426c53d7c29464c33ab4d4
6fears7 <57415489+6fears7@users.noreply.github.com>
2025-10-04 06:00:54 -0400

    add nico

--- RainMeadow.cs
+++ RainMeadow.cs
@@ -273,7 +273,8 @@ namespace RainMeadow
             "PwcZS6t8kETyBdrPiR2ple35lpLMfEw6TP/VyHVD4z4=",
             "wZ2+Phw6EOBLv9bZKdSGV+3lWhNxiT2KHwCluqhLdzo=",
             "Hr8BfOHHTBRGgSmQoj4qQdlHqaY6d4DHFbF7wCNFI1U=",
-            "cOL0sHXOvRyn7y5S+3VXWmuyZE1KvQXdfBgcHrph2kE="
+            "cOL0sHXOvRyn7y5S+3VXWmuyZE1KvQXdfBgcHrph2kE=",
+            "3aA5+Ga/lMY848/EcCZLBnO93TS1RhPfSMgAGtf7MQY=
         };
 
         public static bool IsDev(MeadowPlayerId player)
