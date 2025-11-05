2f11fca7f7811b09eefbcde8f6b623601247e0ce
6fears7 <6fears7even@gmail.com>
2025-11-05 14:41:11 -0500

    init for sand grubs, more work to do

---
+++ Online/State/RealizedSandGrubState.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using Watcher;
+
+namespace RainMeadow
+{
+    public class RealizedSandGrubState : RealizedCreatureState
+    {
+
+
+        [OnlineFieldHalf]
+        public float tailLength;
+        [OnlineFieldHalf]
+        public float lastTetherLength;
+        [OnlineFieldHalf]
+        public float tetherLength;
+        [OnlineFieldHalf]
+        public float lastMouthOpen;
+        [OnlineField]
+        int bites;
+        [OnlineField]
+        public int buryCounter;
+        [OnlineField]
+        public int tentacleHuntCounter;
+        public RealizedSandGrubState() { }
+        public RealizedSandGrubState(OnlineCreature onlineEntity) : base(onlineEntity)
+        {
+            SandGrub grub = (SandGrub)onlineEntity.realizedCreature;
+            tailLength = grub.tailLength;
+            tetherLength = grub.tetherLength;
+            lastTetherLength = grub.lastTetherLength;
+            lastMouthOpen = grub.lastMouthOpen;
+            bites = grub.BitesLeft;
+            buryCounter = grub.buryCounter;
+
+
+        }
+
+        public override void ReadTo(OnlineEntity onlineEntity)
+        {
+            base.ReadTo(onlineEntity);
+            if ((onlineEntity as OnlineCreature).apo.realizedObject is not SandGrub grub) { RainMeadow.Error("target not realized: " + onlineEntity); return; }
+
+            grub.tailLength = tailLength;
+            grub.tetherLength = tetherLength;
+            grub.lastTetherLength = lastTetherLength;
+            grub.lastMouthOpen = lastMouthOpen;
+            grub.BitesLeft = bites;
+            grub.tentacleHuntCounter = tentacleHuntCounter;
+            grub.buryCounter = buryCounter;
+            grub.ai.behavior = SandGrubAI.Behavior.Hide;
+           
+        }
+    }
+}
+
