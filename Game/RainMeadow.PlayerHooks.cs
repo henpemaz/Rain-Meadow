088bfcb5c8cec7b581cd5e66b8fe236a0136db10
6fears7 <57415489+6fears7@users.noreply.github.com>
2026-02-21 18:30:37 -0700

    Update RainMeadow.PlayerHooks.cs

--- Game/RainMeadow.PlayerHooks.cs
+++ Game/RainMeadow.PlayerHooks.cs
@@ -821,13 +821,12 @@ public partial class RainMeadow
     // Hide the Meadow mode slugcat so it doesn't appear in menus (e.g. arena)
     private bool SlugcatStatsOnHiddenOrUnplayableSlugcat(On.SlugcatStats.orig_HiddenOrUnplayableSlugcat orig, SlugcatStats.Name i)
     {
-        if (i == Ext_SlugcatStatsName.OnlineSessionPlayer)
-        {
+        if (Ext_SlugcatStatsName.AllMeadowNames.Contains(i))
+          {
             return true;
-        }
+          }
 
         return orig(i);
-    }
 
     private void SlugOnBack_Update(On.Player.SlugOnBack.orig_Update orig, Player.SlugOnBack self, bool eu)
     {
