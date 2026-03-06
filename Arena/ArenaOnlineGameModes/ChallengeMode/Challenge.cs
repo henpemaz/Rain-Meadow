521107a8aee309d78c3ffb37325ecb6325c28b20
6fears7 <6fears7even@gmail.com>
2026-03-06 11:30:24 -0700

    add score hud

--- Arena/ArenaOnlineGameModes/ChallengeMode/Challenge.cs
+++ Arena/ArenaOnlineGameModes/ChallengeMode/Challenge.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using HUD;
 using Menu;
 using RainMeadow;
 using RainMeadow.Arena.ArenaOnlineGameModes.TeamBattle;
@@ -136,10 +137,28 @@ namespace RainMeadow.Arena.ArenaOnlineGameModes.ArenaChallengeModeNS
             return base.IconColor(arena, display, owner, customization, player);
         }
 
+        public override void HUD_InitMultiplayerHud(ArenaOnlineGameMode arena, HUD.HUD self, ArenaGameSession session)
+        {
+            base.HUD_InitMultiplayerHud(arena, self, session);
+            var psmh = new PlayerSpecificMultiplayerHud(self, session, session.Players.FirstOrDefault(x => x.GetOnlineCreature().isMine));
+            psmh.cornerPos = new Vector2(self.rainWorld.options.ScreenSize.x - self.rainWorld.options.SafeScreenOffset.x, 20f + self.rainWorld.options.SafeScreenOffset.y);
+            psmh.flip = -1;
+
+            psmh.parts.Clear();
+            var killsList = new HUD.PlayerSpecificMultiplayerHud.KillList(psmh);
+            var scoreCounter = new HUD.PlayerSpecificMultiplayerHud.ScoreCounter(psmh);
+            scoreCounter.scoreText.color = Color.white; // can't see crap
+            scoreCounter.lightGradient.color = Color.white;
+            psmh.parts.Add(killsList);
+            psmh.parts.Add(scoreCounter);
+            self.AddPart(psmh);
+
+        }
+
         public override Dialog AddGameModeInfo(ArenaOnlineGameMode arena, Menu.Menu menu)
         {
             return new DialogNotify(
-                menu.LongTranslate("Pit yourself against a series of challenges"),
+                menu.LongTranslate("Pit yourself against a series of challenges<LINE>Scoring scales with lobby size<LINE>All players must remain alive to win"),
                 new Vector2(500f, 400f),
                 menu.manager,
                 () =>
