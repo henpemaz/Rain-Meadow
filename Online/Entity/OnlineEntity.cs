85185da45bd7d7282b8a579e89e94bbe3e877d41
6fears7 <6fears7even@gmail.com>
2026-04-04 10:23:20 -0400

    fix: defer deregistration to prevent null entity but present states

--- Online/Entity/OnlineEntity.cs
+++ Online/Entity/OnlineEntity.cs
@@ -252,7 +252,8 @@ namespace RainMeadow
             }
             if (primaryResource == null && !isPending)
             {
-                Deregister();
+                // let those last state packets come in first
+                OnlineManager.RunDeferred(() => Deregister());
             }
         }
 
@@ -395,33 +396,33 @@ namespace RainMeadow
 
         public virtual void ReadState(EntityState entityState, OnlineResource inResource)
         {
-                lastStates[inResource] = entityState;
-                StateProfiler.Instance?.Push(entityState.GetType());
-                entityState.ReadTo(this);
-                StateProfiler.Instance?.Pop(entityState.GetType());
+            lastStates[inResource] = entityState;
+            StateProfiler.Instance?.Push(entityState.GetType());
+            entityState.ReadTo(this);
+            StateProfiler.Instance?.Pop(entityState.GetType());
 
         }
 
-        public virtual bool CanReadTo(EntityState entityState, OnlineResource inResource, uint tick) 
+        public virtual bool CanReadTo(EntityState entityState, OnlineResource inResource, uint tick)
         {
             var entity = entityState.entityId.FindEntity();
             if (entity == null)
             {
                 RainMeadow.Error($"CanReadTo: Entity state cannot readto. Reason: entity.FindEntity() is null: {entityState.from}, {entityState.entityId}");
                 return false;
-            } 
+            }
 
             var localState = entity.GetState(tick, inResource);
             if (localState == null)
             {
                 RainMeadow.Error($"CanReadTo: Entity state cannot readto. Reason: localState.GetState() is null");
                 return false;
-            } 
+            }
 
 
             if (!entityState.GetType().IsAssignableFrom(localState.GetType()))
             {
-            RainMeadow.Error($"CanReadTo: Is not assignable EntityState: {entityState.GetType()}, localState: {localState.GetType()}:  {entityState.GetType().IsAssignableFrom(localState.GetType())}");
+                RainMeadow.Error($"CanReadTo: Is not assignable EntityState: {entityState.GetType()}, localState: {localState.GetType()}:  {entityState.GetType().IsAssignableFrom(localState.GetType())}");
             }
 
             return entityState.GetType().IsAssignableFrom(localState.GetType());
