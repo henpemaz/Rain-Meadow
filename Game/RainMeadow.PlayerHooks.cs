80418da42eb4688952faca792abde114f1ab2ee4
6fears7 <6fears7even@gmail.com>
2026-04-15 06:05:12 -0400

    fix:  arti capacity hook not working

--- Game/RainMeadow.PlayerHooks.cs
+++ Game/RainMeadow.PlayerHooks.cs
@@ -2260,7 +2260,7 @@ public partial class RainMeadow
             c.Index = 0;
             // looking for the call to get_Value() from the cfgArtificerExplosionCapacity config
             while (c.TryGotoNext(MoveType.After,
-                i => i.MatchLdsfld<MoreSlugcats.MoreSlugcats>("cfgArtificerExplosionCapacity")
+                i => i.MatchLdsfld<MoreSlugcats.MoreSlugcats>(nameof(MoreSlugcats.MoreSlugcats.cfgArtificerExplosionCapacity))
             ))
             {
                 // ignore type definition just override the value
