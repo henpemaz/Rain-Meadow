a2f62b68d9949fa44b84a456cb9b862f5d583607
6fears7 <6fears7even@gmail.com>
2026-08-11 05:40:15 -0500

    fix: set levels dir as high impact

--- ModManager/RainMeadowModInfoManager.cs
+++ ModManager/RainMeadowModInfoManager.cs
@@ -104,8 +104,8 @@ public static class RainMeadowModInfoManager
         var modInfo = new RainMeadowModInfo();
 
         // TODO: consider mod.modifiesRegions
-        // Mods that change the world regions or the arena levels are high-impact: clients missing
-        // them would crash on level join .
+        // Mods that change the world regions or the arena levels are high-impact
+        // For arena, clients missing them would crash on level join .
         string[] highImpactDirectories =
         [
             Path.Combine(mod.path, "modify", "world"),
