ddea7838efe476625a991c9551f72831c5c70d3e
6fears7 <57415489+6fears7@users.noreply.github.com>
2026-08-16 14:23:52 -0500

    Update Arena/ArenaPlayerStats.cs
    Co-authored-by: Noah Hewitt <52514366+invalidunits@users.noreply.github.com>

--- Arena/ArenaPlayerStats.cs
+++ Arena/ArenaPlayerStats.cs
@@ -19,6 +19,8 @@ namespace RainMeadow
         public List<IconSymbol.IconSymbolData> allKills = [];
         public List<IconSymbol.IconSymbolData> roundKills = [];
 
+public ushort ID => inLobbyId;
+
         public ArenaPlayerStats() { }
 
         /// <summary>
