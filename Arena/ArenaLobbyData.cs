ae2a6c76bb74970010f36453b6062a7085338395
Henrique Maziero <henpemaz@gmail.com>
2026-08-18 13:23:56 +0200

    Longlist support wip

--- Arena/ArenaLobbyData.cs
+++ Arena/ArenaLobbyData.cs
@@ -46,7 +46,7 @@ namespace RainMeadow
             public List<int> bannedSlugs;
 
 
-            [OnlineField(group = "arenaSetup")]
+            [OnlineField(group = "arenaSetup", longList = true)]
             public List<string> playList;
 
             [OnlineField(group = "arenaSetup")]
@@ -175,7 +175,7 @@ namespace RainMeadow
             public bool artiParryLeniency;
 
 
-            [OnlineField(group = "arenaGameplay")]
+            [OnlineField(group = "arenaGameplay", longList = true)]
             public List<ushort> arenaSittingOnlineOrder;
 
             [OnlineField(group = "arenaGameplay")]
@@ -209,7 +209,7 @@ namespace RainMeadow
             [OnlineField(group = "arenaScore")]
             public Dictionary<int, int> scoreByInLobbyId;
 
-            [OnlineField(group = "arenaScore")]
+            [OnlineField(group = "arenaScore")] // TODO make it's inner-list serialize as long-list
             public Dictionary<int, List<string>> allKillsByInLobbyId;
 
             [OnlineField(group = "arenaScore")]
