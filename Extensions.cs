2f26c85a4afb259618e03594f411cb8c668f18bd
iiul <iulh@users.noreply.github.com>
2026-08-30 14:54:04 +0300

    fix: `IsLocal` throws error on no lobby

--- Extensions.cs
+++ Extensions.cs
@@ -50,13 +50,17 @@ namespace RainMeadow
         public static bool GetOnlineCreature(this AbstractCreature apo, out OnlineCreature oc) => (oc = GetOnlineCreature(apo) ?? default!) is not null;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsLocal(this AbstractPhysicalObject apo) => OnlineManager.lobby is null || (GetOnlineObject(apo)?.isMine ?? true);
+        public static bool IsLocal(this AbstractPhysicalObject apo)
+        {
+            if (OnlineManager.lobby is null) throw new InvalidOperationException("The lobby is null, did you forgeet to check for a valid lobby?");
+            if (GetOnlineObject(apo) is not OnlinePhysicalObject opo) return true;
+            return opo.isMine;
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsLocal(this AbstractPhysicalObject apo, out OnlinePhysicalObject opo)
         {
-            opo = default!;
-            if (OnlineManager.lobby is null) return false;
+            if (OnlineManager.lobby is null) throw new InvalidOperationException("The lobby is null, did you forgeet to check for a valid lobby?");
             return OnlinePhysicalObject.map.TryGetValue(apo, out opo) && opo.isMine;
         }
 
